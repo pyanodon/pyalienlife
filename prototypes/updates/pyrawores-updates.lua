8f40718cbbfe3f40a4f9b375f2b6354530146737
pyanodon <smylodon@gmail.com>
2019-05-30 19:40:32 -0300

    moss2
    Former-commit-id: 87c9da1e7328c091f2e6b5176cfe4a596112d8af
    Former-commit-id: 1ceb2a2e1c5522a1b0e8856ccd8fdd041db738f9

--- prototypes/updates/pyrawores-updates.lua
+++ prototypes/updates/pyrawores-updates.lua
@@ -1,5 +1,6 @@
 --BUILDINDS--
 
 --RECIPES--
+RECIPE('moss-farm-mk02'):add_ingredient({type = "item", name = "duralumin", amount = 10}):add_ingredient({type = "item", name = "glass", amount = 50})
 
 ----EXCLUSIVE RECIPES----
