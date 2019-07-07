a4f710dd004fe06d28fd32de4ad3689ce3d452ad
pyanodon <smylodon@gmail.com>
2019-07-07 12:50:59 -0300

    added genlab and imported the incubator

---
+++ prototypes/buildings/genlab-mk01.lua
@@ -0,0 +1,173 @@
+RECIPE {
+    type = "recipe",
+    name = "genlab-mk01",
+    energy_required = 1,
+    enabled = false,
+    ingredients = {
+        {"electrolyzer-mk01", 1},
+        {"plastic-bar", 20},
+        {"py-tank-3000", 2},
+        {"electronic-circuit", 15},
+        {"glass", 50},
+        {"titanium-plate", 40},
+    },
+    results = {
+        {"genlab-mk01", 1}
+    }
+}:add_unlock("genetics-mk01")
+
+ITEM {
+    type = "item",
+    name = "genlab-mk01",
+    icon = "__pyalienlife__/graphics/icons/genlab-mk01.png",
+    icon_size = 64,
+    flags = {},
+    subgroup = "py-alienlife-buildings-mk01",
+    order = "c",
+    place_result = "genlab-mk01",
+    stack_size = 10
+}
+
+ENTITY {
+    type = "assembling-machine",
+    name = "genlab-mk01",
+    icon = "__pyalienlife__/graphics/icons/genlab-mk01.png",
+	icon_size = 64,
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {mining_time = 0.5, result = "genlab-mk01"},
+    fast_replaceable_group = "genlab",
+    max_health = 100,
+    corpse = "medium-remnants",
+    dying_explosion = "big-explosion",
+    collision_box = {{-3.1, -3.1}, {3.1, 3.1}},
+    selection_box = {{-3.5, -3.5}, {3.5, 3.5}},
+    module_specification = {
+        module_slots = 1
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"},
+    crafting_categories = {"genlab"},
+    crafting_speed = 1,
+    energy_source = {
+        type = "electric",
+        usage_priority = "secondary-input",
+        emissions_per_minute = 1,
+    },
+    energy_usage = "300kW",
+    ingredient_count = 10,
+    animation = {
+        layers = {
+            {
+                filename = "__pyalienlife__/graphics/entity/genlab/base.png",
+                width = 256,
+                height = 32,
+                line_length = 8,
+                frame_count = 100,
+                animation_speed = 0.4,
+                shift = util.by_pixel(16, 96)
+            },
+            {
+                filename = "__pyalienlife__/graphics/entity/genlab/a1.png",
+                width = 128,
+                height = 288,
+                line_length = 16,
+                frame_count = 100,
+                animation_speed = 0.4,
+                shift = util.by_pixel(-48, -64)
+            },
+            {
+                filename = "__pyalienlife__/graphics/entity/genlab/a1-mask.png",
+                width = 128,
+                height = 288,
+                line_length = 16,
+                frame_count = 100,
+                animation_speed = 0.4,
+                shift = util.by_pixel(-48, -64),
+                tint = {r = 1.0, g = 1.0, b = 0.0, a = 1.0}
+            },
+            {
+                filename = "__pyalienlife__/graphics/entity/genlab/a2.png",
+                width = 128,
+                height = 288,
+                line_length = 16,
+                frame_count = 100,
+                animation_speed = 0.4,
+                shift = util.by_pixel(80, -64)
+            },
+            {
+                filename = "__pyalienlife__/graphics/entity/genlab/a2-mask.png",
+                width = 128,
+                height = 288,
+                line_length = 16,
+                frame_count = 100,
+                animation_speed = 0.4,
+                shift = util.by_pixel(80, -64),
+                tint = {r = 1.0, g = 1.0, b = 0.0, a = 1.0}
+            },
+        }
+    },
+    fluid_boxes = {
+        --1
+        {
+            production_type = "input",
+            pipe_covers = DATA.Pipes.covers(false, true, true, true),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-3", nil, {0.0, -0.88}, nil, nil),
+            base_area = 10,
+            base_level = -1,
+            pipe_connections = {{type = "input", position = {0.0, -4.0}}},
+            secondary_draw_orders = { north = -1 }
+        },
+        {
+            production_type = "input",
+            pipe_covers = DATA.Pipes.covers(false, true, true, true),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-3", nil, {0.0, -0.88}, nil, nil),
+            base_area = 10,
+            base_level = -1,
+            pipe_connections = {{type = "input", position = {-2.0, -4.0}}},
+            secondary_draw_orders = { north = -1 }
+        },
+        {
+            production_type = "input",
+            pipe_covers = DATA.Pipes.covers(false, true, true, true),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-3", nil, {0.0, -0.88}, nil, nil),
+            base_area = 10,
+            base_level = -1,
+            pipe_connections = {{type = "input", position = {2.0, -4.0}}},
+            secondary_draw_orders = { north = -1 }
+        },
+        {
+            production_type = "output",
+            pipe_covers = DATA.Pipes.covers(false, true, true, true),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-3", nil, {0.0, -0.88}, nil, nil),
+            base_area = 10,
+            base_level = 1,
+            pipe_connections = {{type = "output", position = {0.0, 4.0}}},
+            secondary_draw_orders = { north = -1 }
+        },
+        {
+            production_type = "output",
+            pipe_covers = DATA.Pipes.covers(false, true, true, true),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-3", nil, {0.0, -0.88}, nil, nil),
+            base_area = 10,
+            base_level = 1,
+            pipe_connections = {{type = "output", position = {-2.0, 4.0}}},
+            secondary_draw_orders = { north = -1 }
+        },
+        {
+            production_type = "output",
+            pipe_covers = DATA.Pipes.covers(false, true, true, true),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-3", nil, {0.0, -0.88}, nil, nil),
+            base_area = 10,
+            base_level = 1,
+            pipe_connections = {{type = "output", position = {2.0, 4.0}}},
+            secondary_draw_orders = { north = -1 }
+        },
+        off_when_no_fluid_recipe = true
+    },
+
+    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
+    working_sound = {
+        sound = {filename = "__pyalienlife__/sounds/genlab.ogg", volume = 1.5},
+        idle_sound = {filename = "__pyalienlife__/sounds/genlab.ogg", volume = 1.1},
+        apparent_volume = 2.5
+    }
+}
---
+++ prototypes/technologies/genetics.lua
@@ -0,0 +1,78 @@
+TECHNOLOGY {
+    type = "technology",
+    name = "genetics-mk01",
+    icon = "__pyalienlife__/graphics/technology/genetics-mk01.png",
+    icon_size = 128,
+    order = "c-a",
+    prerequisites = {"coal-processing-2","microbiology-mk02"},
+    effects = {},
+    unit = {
+        count = 150,
+        ingredients = {
+            {"automation-science-pack", 1},
+            {'logistic-science-pack', 1}
+        },
+        time = 45
+    }
+}
+
+TECHNOLOGY {
+    type = 'technology',
+    name = 'genetics-mk02',
+    icon = '__pyalienlife__/graphics/technology/genetics-mk02.png',
+    icon_size = 128,
+    order = 'c-a',
+    prerequisites = {'genetics-mk01'},
+    effects = {},
+    unit = {
+        count = 150,
+        ingredients = {
+            {'automation-science-pack', 1},
+            {'logistic-science-pack', 1},
+            {'chemical-science-pack', 1},
+        },
+        time = 45
+    }
+}
+
+TECHNOLOGY {
+    type = 'technology',
+    name = 'genetics-mk03',
+    icon = '__pyalienlife__/graphics/technology/genetics-mk03.png',
+    icon_size = 128,
+    order = 'c-a',
+    prerequisites = {'genetics-mk02'},
+    effects = {},
+    unit = {
+        count = 150,
+        ingredients = {
+            {'automation-science-pack', 1},
+            {'logistic-science-pack', 1},
+            {'utility-science-pack', 1},
+            {'chemical-science-pack', 1},
+            
+        },
+        time = 60
+    }
+}
+
+TECHNOLOGY {
+    type = "technology",
+    name = "genetics-mk04",
+    icon = "__pyalienlife__/graphics/technology/genetics-mk04.png",
+    icon_size = 128,
+    order = "c-a",
+    prerequisites = {"genetics-mk03"},
+    effects = {},
+    unit = {
+        count = 130,
+        ingredients = {
+            {'automation-science-pack', 3},
+            {'logistic-science-pack', 2},
+            {'chemical-science-pack', 1},
+            {'utility-science-pack', 1},
+            {'production-science-pack', 1}
+        },
+        time = 45
+    }
+}
