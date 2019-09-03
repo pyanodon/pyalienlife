a1e38508061887cd7d9028f5b123f0dc36aa86d8
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-09-03 04:52:01 -0400

    rewrote autorender to handle single and dual recipes
    Former-commit-id: 086c6f82f544fe04aaff5addc9f314349fbfa726
    Former-commit-id: a8a63d27b8ffae0ab052b483491d7a1b151db77b

---
+++ prototypes/recipes/itemtables.lua
@@ -0,0 +1,15 @@
+local items = {}
+items.inputs = {}
+items.outputs = {}
+--table.itemkey = {item, default item amount}
+
+items.inputs.a = {'soil', 2}
+items.inputs.b = {'water', 25}
+items.inputs.c = {'ash', 5}
+items.inputs.d = {'wood', 4}
+items.inputs.e = {'solid-fuel', 2}
+
+items.outputs.a = {'ralesia', 1}
+items.outputs.b = {'zipir',2}
+
+return items
