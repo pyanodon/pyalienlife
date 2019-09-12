7415c2f21a7ecb0c63b7d799b1d593fef6c10bb2
pyanodon <smylodon@gmail.com>
2019-09-12 19:23:07 -0300

    changes
    Former-commit-id: dc452b4550d8d3d54d96d9d8707a92960cca7543
    Former-commit-id: f271d46e9d991478836e1374487ee8ff375ad88d

--- prototypes/recipes/recipes-auto-ulric.lua
+++ prototypes/recipes/recipes-auto-ulric.lua
@@ -21,7 +21,7 @@ fun.autorecipes {
 			results =
 				{
 					{'bones',4/10,a_min=1,a_max=3},
-					{'meat',4/10,a_min=1,a_max=3},
+					{'meat',4/10,a_min=1,a_max=1},
 					{'fat',3/10,a_min=1,a_max=1},
 					{'brain',4/10,a_min=1,a_max=1},
 					{'guts',4/10,a_min=1,a_max=1},
@@ -29,15 +29,79 @@ fun.autorecipes {
                     {'blood',50},
 				},
 				crafting_speed = 130,
-				tech = 'ulric'
+				out_crafting_speed = 30,
+				tech = 'ulric',
+				newitem = false,
+				name = 'full rendering ulric'
         },
 		{
 			ingredients =
 				{
-					--{'cage'},
-                    {'ulricfood01','R'},
-                    {'ulricfood02'},
-                    --{'waterbarrel',3},
+					{'renneaseeds',25},
+				},
+			results =
+				{
+					{'bones','+1/10',icon=true},
+					{'meat','+1/10'},
+					{'brain','+1/10'},
+					{'bonemeal','+1'},
+				},
+				crafting_speed = 110,
+				tech = 'ulric',
+				name='boney ulric'
+		},
+		{
+			ingredients =
+				{
+					{'fungalsubstrate'},
+				},
+			results =
+				{
+					{'bones','+1/10'},
+					{'meat','+1/10',icon=true},
+					{'brain','+1/10'},
+					{'bonemeal','+1'},
+				},
+				crafting_speed = 110,
+				tech = 'ulric',
+				name = 'Mealy Ulric'
+		},
+		{
+			ingredients =
+				{
+					{'slackedlime'}
+				},
+			results =
+				{
+					{'bones','+1/10'},
+					{'meat','+1/10'},
+					{'brain','+1/10',icon=true},
+					{'bonemeal','+1'},
+				},
+				crafting_speed = 110,
+				tech = 'ulric',
+				newitem = true,
+				name = 'brainy Ulric'
+		},
+		{
+			ingredients =
+				{
+					{'fertilizer'}
+				},
+			results =
+				{
+					{'bones','+1/10'},
+					{'meat','+1/10'},
+					{'brain','+1/10'},
+					{'bonemeal','+1'},
+				},
+				crafting_speed = 110,
+				tech = 'ulric'
+		},
+		{
+			ingredients =
+				{
+					{'rawfiber',30}
 				},
 			results =
 				{
