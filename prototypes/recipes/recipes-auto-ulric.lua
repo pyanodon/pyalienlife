ec16dd91a64d1d1961fff27571051d58187caa6c
pyanodon <smylodon@gmail.com>
2019-09-15 13:20:59 -0300

    tests
    Former-commit-id: de2347df9fad85daa37e69da5d6f8fc1ec92a65f
    Former-commit-id: 529f61ecc7e6bcaf86b4544aca6ecf25eba8da7a

--- prototypes/recipes/recipes-auto-ulric.lua
+++ prototypes/recipes/recipes-auto-ulric.lua
@@ -17,7 +17,7 @@ fun.autorecipes {
 				{
 					{name='cagedulric', probability = 0.5,amount_min =1,amount_max =1},
 				},
-			crafting_speed = 130,
+			crafting_speed = 150,
 			tech = 'ulric'
 		},
 		--food 1
@@ -29,26 +29,80 @@ fun.autorecipes {
 				},
 			results =
 				{
-
+				
 				},
-			crafting_speed = 110,
+			crafting_speed = 140,
 			tech = 'ulric'
 		},
+		--salt
+		{
+			ingredients =
+				{
+					{name='salt',amount = 5},
+				},
+			results =
+				{
+				
+				},
+			crafting_speed = 130,
+			tech = 'ulric'
+		},
+		--fiber
+		{
+			ingredients =
+				{
+					{name='rawfiber',amount = 5},
+				},
+			results =
+				{
+				
+				},
+			crafting_speed = 120,
+			tech = 'ulric'
+		},		
 		--food 2
 		{
 			ingredients =
 				{
 					{name='ulricfood01',amount ='R'},
+					{name='salt',amount ='R'},
+					{name='rawfiber',amount ='R'},
 					{name='ulricfood02',amount =1},
 
 				},
 			results =
 				{
-
+				
 				},
-			crafting_speed = 80,
+			crafting_speed = 110,
 			tech = 'ulric'
 		},
+		--food 2 salt
+		{
+			ingredients =
+				{
+					{name='salt',amount ='R'},
+				},
+			results =
+				{
+				
+				},
+			crafting_speed = 100,
+			tech = 'ulric'
+		},
+		--food 2 salt fiber
+		{
+			ingredients =
+				{
+					{name='rawfiber',amount = 5},
+				},
+			results =
+				{
+				
+				},
+			crafting_speed = 90,
+			tech = 'ulric'
+		},	
 		--gh
 		{
 			ingredients =
@@ -58,10 +112,188 @@ fun.autorecipes {
 				},
 			results =
 				{
-
+				
 				},
 			crafting_speed = 30,
 			tech = 'growth-hormone'
 		},
+	}
+}
+
+
+
+------------------RENDERING------------------
+
+fun.autorecipes {
+    name = 'rendering',
+	category = 'slaughterhouse',
+	--module_limitations = 'ulric',
+	subgroup = 'py-alienlife-ulric',
+	order = 'b',
+    mats =
+	{
+		{
+			ingredients =
+				{
+					{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='bones', probability = 0.4,amount_min =1,amount_max =3},
+					{name='meat', probability = 0.4,amount_min =1,amount_max =3},
+					{name='skin', probability = 0.2,amount_min =1,amount_max =1},
+					{name='fat', probability = 0.3,amount_min =1,amount_max =1},
+					{name='guts', probability = 0.4,amount_min =1,amount_max =1},
+					{name='blood', amount =50},
+					{name='brain', probability = 0.4,amount_min =1,amount_max =1},
+				},
+			crafting_speed = 30,
+			tech = 'ulric',
+			name = 'Full Render Ulrics',
+			icon = "__pyalienlife__/graphics/icons/rendering-ulric.png",
+			icon_size = 64,
+		},
+		--bonemeal
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='bones', amount ='R'},
+					{name='meat', amount ='R'},
+					{name='skin', amount ='R'},
+					{name='fat', amount ='R'},
+					{name='guts', amount ='R'},
+					{name='blood', amount ='R'},
+					{name='brain', amount ='R'},
+					{name='bonemeal', amount = 6},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Ulrics to Bonemeal',
+			icon = "__pyalienlife__/graphics/icons/bonemeal.png",
+			icon_size = 64,
+		},
+		--meat
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='bonemeal', amount ='R'},
+					{name='meat', amount =4},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric Meat',
+			icon = "__pyalienlife__/graphics/icons/mip/meat-01.png",
+			icon_size = 64,
+		},
+		--brain
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='meat', amount ='R'},
+					{name='brain', amount =1},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric brains',
+			icon = "__pyalienlife__/graphics/icons/mip/brain-04.png",
+			icon_size = 64,
+		},	
+		--skin
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='brain', amount ='R'},
+					{name='skin', amount =2},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric skin',
+			icon = "__pyalienlife__/graphics/icons/skin.png",
+			icon_size = 32,
+		},
+		--bones
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='skin', amount ='R'},
+					{name='bones', amount =5},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric bones',
+			icon = "__pyalienlife__/graphics/icons/mip/bones-01.png",
+			icon_size = 64,
+		},
+		--guts
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='bones', amount ='R'},
+					{name='guts', amount =3},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric guts',
+			icon = "__pyalienlife__/graphics/icons/mip/guts-01.png",
+			icon_size = 64,
+		},
+		--blood
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='guts', amount ='R'},
+					{name='blood', amount =80},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric blood',
+			icon = "__pyalienlife__/graphics/icons/blood.png",
+			icon_size = 64,
+		},
+		--fat
+		{
+			ingredients =
+				{
+					--{name='cagedulric',amount=1},
+				},
+			results =
+				{
+					{name='blood', amount ='R'},
+					{name='fat', amount =2},
+				},
+			crafting_speed = 15,
+			tech = 'ulric',
+			name = 'Extract Ulric lard',
+			icon = "__pyalienlife__/graphics/icons/mukmoux-fat.png",
+			icon_size = 64,
+		},	
 	}
 }
