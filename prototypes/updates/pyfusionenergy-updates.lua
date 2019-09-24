aa592d9b560a28fc926d089c43700181f2803d88
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-09-23 20:39:56 -0400

    readded machine unlocks

--- prototypes/updates/pyfusionenergy-updates.lua
+++ prototypes/updates/pyfusionenergy-updates.lua
@@ -6,7 +6,7 @@ RECIPE('genlab-mk01'):remove_unlock('fusion-mk01'):remove_unlock('nano-tech')
 RECIPE('bio-reactor'):remove_unlock('helium-processing'):remove_unlock('nano-tech'):add_unlock('genetics-mk01'):replace_ingredient("gasturbinemk02", "gasturbinemk01"):replace_ingredient("super-alloy", "lead-plate"):replace_ingredient("advanced-circuit", "electronic-circuit"):subgroup_order("py-alienlife-buildings-others", "a"):set_fields {crafting_speed = 1}
 RECIPE('ralesia-plantation-mk03'):add_ingredient({type = "item", name = "super-alloy", amount = 10}):add_ingredient({type = "item", name = "boron-carbide", amount = 20})
 RECIPE('ralesia-plantation-mk04'):add_ingredient({type = "item", name = "nbti-alloy", amount = 15}):add_ingredient({type = "item", name = "wall-shield", amount = 8}):add_ingredient({type = "item", name = "science-coating", amount = 1}):add_ingredient({type = "item", name = "control-unit", amount = 5})
-RECIPE('xyhiphoe-pool-mk01'):remove_unlock('vanadium-processing')
+RECIPE('xyhiphoe-pool-mk01'):remove_unlock('vanadium-processing'):add_unlock('water-invertebrates-mk03'):add_ingredient({type = "item", name = "nexelit-plate", amount = 20}):add_ingredient({type = "item", name = "iron-gear-wheel", amount = 40}):subgroup_order('py-alienlife-buildings-others', 'b')
 RECIPE('xyhiphoe-pool-mk02'):remove_unlock('machines-mk02')
 RECIPE('xyhiphoe-pool-mk03'):remove_unlock('machines-mk03')
 RECIPE('xyhiphoe-pool-mk04'):remove_unlock('machines-mk04')
