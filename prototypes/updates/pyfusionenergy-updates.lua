c5a91f2d4f7fedd7d1150f0d8dc330c0dadb9feb
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-09-23 20:30:19 -0400

    Revert "Revert "updated pyfusion building names""
    This reverts commit 6c129f1475fa66704937a37a6ed27834194c766b.

--- prototypes/updates/pyfusionenergy-updates.lua
+++ prototypes/updates/pyfusionenergy-updates.lua
@@ -2,11 +2,11 @@
 
 --BUILDINDS--
 RECIPE('plankton-farm'):remove_unlock('vanadium-processing'):add_unlock('microbiology-mk01'):remove_ingredient("niobium-plate"):remove_ingredient("storage-tank"):add_ingredient({type = "item", name = "pipe", amount = 20}):add_ingredient({type = "item", name = "iron-gear-wheel", amount = 25}):subgroup_order('py-alienlife-buildings-others', 'b')
-RECIPE('genlab'):remove_unlock('fusion-mk01'):remove_unlock('nano-tech')
+RECIPE('genlab-mk01'):remove_unlock('fusion-mk01'):remove_unlock('nano-tech')
 RECIPE('bio-reactor'):remove_unlock('helium-processing'):remove_unlock('nano-tech'):add_unlock('genetics-mk01'):replace_ingredient("gasturbinemk02", "gasturbinemk01"):replace_ingredient("super-alloy", "lead-plate"):replace_ingredient("advanced-circuit", "electronic-circuit"):subgroup_order("py-alienlife-buildings-others", "a"):set_fields {crafting_speed = 1}
 RECIPE('ralesia-plantation-mk03'):add_ingredient({type = "item", name = "super-alloy", amount = 10}):add_ingredient({type = "item", name = "boron-carbide", amount = 20})
 RECIPE('ralesia-plantation-mk04'):add_ingredient({type = "item", name = "nbti-alloy", amount = 15}):add_ingredient({type = "item", name = "wall-shield", amount = 8}):add_ingredient({type = "item", name = "science-coating", amount = 1}):add_ingredient({type = "item", name = "control-unit", amount = 5})
-RECIPE('xyhiphoe-pool'):remove_unlock('vanadium-processing'):add_unlock('water-invertebrates-mk03'):add_ingredient({type = "item", name = "nexelit-plate", amount = 20}):add_ingredient({type = "item", name = "iron-gear-wheel", amount = 40}):subgroup_order('py-alienlife-buildings-others', 'b')
+RECIPE('xyhiphoe-pool-mk01'):remove_unlock('vanadium-processing'):add_unlock('water-invertebrates-mk03'):add_ingredient({type = "item", name = "nexelit-plate", amount = 20}):add_ingredient({type = "item", name = "iron-gear-wheel", amount = 40}):subgroup_order('py-alienlife-buildings-others', 'b')
 
 ----RECIPES----
 RECIPE('phytoplankton'):remove_unlock('vanadium-processing'):add_unlock('microbiology-mk01'):subgroup_order('py-alienlife-plants', 'b')
@@ -20,7 +20,7 @@ ITEM('kmauts-ration'):subgroup_order('py-alienlife-food', 'b')
 
 RECIPE('xyhiphoe-blood'):remove_unlock('vanadium-processing')
 RECIPE('vanabins'):remove_ingredient("xyhiphoe-blood"):add_ingredient({type = "fluid", name = "arthropod-blood", amount = 40})
-RECIPE('kmauts-enclosure'):remove_unlock('helium-processing') --substituir por versao pyal
+RECIPE('kmauts-enclosure-mk01'):remove_unlock('helium-processing') --substituir por versao pyal
 RECIPE('kmauts-ration'):subgroup_order('py-alienlife-food', 'b'):remove_unlock('helium-processing'):add_unlock('food-mk03'):add_ingredient({type = "item", name = "seaweed", amount = 10}):add_ingredient({type = "item", name = "meat", amount = 20}):add_ingredient({type = "item", name = "moss", amount = 20})
 :add_ingredient({type = "item", name = "yotoi-seeds", amount = 10}):add_ingredient({type = "item", name = "biomass", amount = 10})
 
