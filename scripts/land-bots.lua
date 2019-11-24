235edc8a91dcafea329af126a800c49ae8524566
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-11-24 05:53:27 -0500

    bits of land bots and farm stuff
    Former-commit-id: 2b6859e7d794ea232d6f91923fe52564a5084087
    Former-commit-id: 96bbb8c2df798e7e41aaff80a95c6afa36bf9dfd

--- scripts/land-bots.lua
+++ scripts/land-bots.lua
@@ -1,7 +1,27 @@
+--[[
 
+global.landbots =
+	{
+		towerid =
+			{
+				requestors =
+					{
+					
+					},
+				providers =
+					{
+					
+					},
+				botstorage,
+				totalbots,
+				maxcontrolablebots,
+			}
+		
+
+]]--
 script.on_init(function(event)
     
-
+global.landbots = {}
 
 end)
 
@@ -9,13 +29,27 @@ script.on_event({defines.events.on_player_built, defines.events.on_robot_built},
 
 local E = event.created_entity
 
+log(E.name)
+
 --swap the roboport for the control tower entity
 	if E.name == 'hidden-roboport-for-logistics-radius' then
 	
-		game.surfaces["nauvis"].create_entity{name="lb-control-tower",position={E.position.x+cbox,E.position.y},--[[direction=defines.direction.west,]] force=E.force}
+		game.surfaces["nauvis"].create_entity{name="lb-control-tower",position={E.position.x+cbox,E.position.y},force=E.force}
 		E.destory()
 	
 	end
+
+end)
+
+script.on_event(defines.events.on_put_item, function(event)
+
+
+
+end)
+
+script.on_event(defines.events.on_built_entity, function(event)
+
+log(event.created_entity.name)
 
 end)
 
