369733500faf392d5c051a76fbee7fd6abc94b2e
pyanodon <smylodon@gmail.com>
2020-01-22 13:03:29 -0300

    fix
    Former-commit-id: 2e840dd76c888f3b6718b24b8603ecb5981a04d8
    Former-commit-id: 265c725a4b408e3675009e43512cccdd78e30cd6

--- prototypes/recipes/recipes-ralesia.lua
+++ prototypes/recipes/recipes-ralesia.lua
@@ -9,7 +9,7 @@ RECIPE {
     enabled = false,
     energy_required = 200,
     ingredients = {
-        {type = 'item', name = 'alien-sample-02', amount = 2},
+        {type = 'item', name = 'alien-sample01', amount = 2},
         {type = 'item', name = 'cdna', amount = 3},
         {type = 'item', name = 'bio-sample', amount = 5},
         {type = 'item', name = 'ralesia-codex', amount = 1},
