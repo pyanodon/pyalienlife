1d170cef613d11e1072611decaad6f6660a8476e
pyanodon <smylodon@gmail.com>
2020-02-15 14:34:05 -0300

    samples requiered
    Former-commit-id: 59b68ff7909acf3ca28f2b7350f6ccaaeb53102a
    Former-commit-id: 7dc84c0261ad03e3f9518a2850b7d99582725314

--- prototypes/recipes/recipes-bhoddos.lua
+++ prototypes/recipes/recipes-bhoddos.lua
@@ -11,6 +11,7 @@ RECIPE {
     ingredients = {
         {type = 'item', name = 'alien-sample-03', amount = 1},
         {type = 'item', name = 'bio-sample', amount = 5},
+        {type = 'item', name = 'earth-shroom-sample', amount = 1},
         {type = 'item', name = 'cdna', amount = 3},
         {type = 'item', name = 'moss-gen', amount = 5},
         {type = 'item', name = 'bhoddos-codex', amount = 1},
