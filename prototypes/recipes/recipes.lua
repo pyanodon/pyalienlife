22dfa0016487f9ec42e039ac226125896ea4ded8
pyanodon <smylodon@gmail.com>
2020-02-16 20:04:43 -0300

    flash

--- prototypes/recipes/recipes.lua
+++ prototypes/recipes/recipes.lua
@@ -1938,7 +1938,7 @@ RECIPE {
         {type = "item", name = "boron-trioxide", amount = 5}
     },
     results = {
-        {"flask", 5}
+        {"flask", 6}
     },
     icon = "__pycoalprocessinggraphics__/graphics/icons/mip/glassware-01.png",
     icon_size = 64,
