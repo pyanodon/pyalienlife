91fb7824d8309c60c7f134ab404e1f4b172b5164
pyanodon <smylodon@gmail.com>
2020-03-09 11:19:28 -0300

    parcial and broken stuff
    Former-commit-id: a9e87f135abf1996713cbb5e6d3e6dba336581b4
    Former-commit-id: 35439c3c4b586e80cff06d560ee26831180cdc03

---
+++ prototypes/buildings/prandium-lab-mk01.lua
@@ -0,0 +1,180 @@
+RECIPE {
+    type = "recipe",
+    name = "prandium-lab-mk01",
+    energy_required = 0.5,
+    enabled = false,
+    ingredients = {
+        {"iron-plate", 100},
+        {"electronic-circuit", 15},
+        {"small-lamp", 8},
+        {"aluminium-plate", 40},
+        {"steel", 40},
+        {"iron-gear-wheel", 30},
+        {"glass", 50},
+    },
+    results = {
+        {"prandium-lab-mk01", 1}
+    }
+}:add_unlock("cottongut-mk01")
+
+ITEM {
+    type = "item",
+    name = "prandium-lab-mk01",
+    icon = "__pyalienlifegraphics__/graphics/icons/prandium-lab-mk01.png",
+    icon_size = 64,
+    flags = {},
+    subgroup = "py-alienlife-buildings-mk01",
+    order = "d",
+    place_result = "prandium-lab-mk01",
+    stack_size = 10
+}
+
+ENTITY {
+    type = "assembling-machine",
+    name = "prandium-lab-mk01",
+    icon = "__pyalienlifegraphics__/graphics/icons/prandium-lab-mk01.png",
+	icon_size = 64,
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {mining_time = 0.5, result = "prandium-lab-mk01"},
+    fast_replaceable_group = "prandium-lab",
+    max_health = 400,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    collision_box = {{-5.7, -5.7}, {5.7, 5.7}},
+    selection_box = {{-6.0, -6.0}, {6.0, 6.0}},
+    match_animation_speed_to_activity = false,
+    module_specification = {
+        module_slots = 20
+    },
+    allowed_effects = {"speed","productivity"},
+    crafting_categories = {"cottongut"},
+    crafting_speed = 0.01,
+    energy_source = {
+        type = "electric",
+        usage_priority = "secondary-input",
+        emissions_per_minute = 1,
+    },
+    energy_usage = "350kW",
+    ingredient_count = 20,
+    animation = {
+        layers = {
+            --TERRAIN
+            {
+                filename = "__pyfusionenergygraphics2__/graphics/entity/prandium-lab/off.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "extra-low",
+            },
+            {
+                filename = "__pyfusionenergygraphics2__/graphics/entity/prandium-lab/off-mask.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "extra-low",
+                tint = {r = 1.0, g = 1.0, b = 0.0, a = 1.0}
+            },
+        }
+    },
+    working_visualisations = {
+        --critters
+        {
+            north_position = util.by_pixel(-0, -0),
+            west_position = util.by_pixel(-0, -0),
+            south_position = util.by_pixel(-0, -0),
+            east_position = util.by_pixel(-0, -0),
+            animation = {
+                filename = "__pyfusionenergygraphics2__/graphics/entity/prandium-lab/a1.png",
+                frame_count = 150,
+                width = 64,
+                height = 320,
+                line_length = 30,
+                animation_speed = 0.4,
+                priority = "low"
+            }
+        },
+        --CENTER
+        {
+            north_position = util.by_pixel(-0, -0),
+            west_position = util.by_pixel(-0, -0),
+            south_position = util.by_pixel(-0, -0),
+            east_position = util.by_pixel(-0, -0),
+            animation = {
+                filename = "__pyfusionenergygraphics2__/graphics/entity/prandium-lab/center.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "medium"
+            }
+        },
+        {
+            north_position = util.by_pixel(-0, -0),
+            west_position = util.by_pixel(-0, -0),
+            south_position = util.by_pixel(-0, -0),
+            east_position = util.by_pixel(-0, -0),
+            animation = {
+                filename = "__pyfusionenergygraphics2__/graphics/entity/prandium-lab/center-mask.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "medium",
+                tint = {r = 1.0, g = 1.0, b = 0.0, a = 1.0}
+            }
+        },
+        --POSTE
+        {
+            north_position = util.by_pixel(-0, -0),
+            west_position = util.by_pixel(-0, -0),
+            south_position = util.by_pixel(-0, -0),
+            east_position = util.by_pixel(-0, -0),
+            animation = {
+                filename = "__pyfusionenergygraphics2__/graphics/entity/prandium-lab/poste.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "medium"
+            }
+        },
+        --FRONT
+        {
+            north_position = util.by_pixel(-0, -0),
+            west_position = util.by_pixel(-0, -0),
+            south_position = util.by_pixel(-0, -0),
+            east_position = util.by_pixel(-0, -0),
+            animation = {
+                filename = "__pyfusionenergygraphics__/graphics/entity/prandium-lab/front.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "extra-high"
+            }
+        },
+        {
+            north_position = util.by_pixel(-0, -0),
+            west_position = util.by_pixel(-0, -0),
+            south_position = util.by_pixel(-0, -0),
+            east_position = util.by_pixel(-0, -0),
+            animation = {
+                filename = "__pyfusionenergygraphics__/graphics/entity/prandium-lab/front-mask.png",
+                width = 384,
+                height = 448,
+                frame_count = 1,
+                shift = {0.0, -0.0},
+                priority = "extra-high",
+                tint = {r = 1.0, g = 1.0, b = 0.0, a = 1.0}
+            }
+        },
+    },
+    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
+    working_sound = {
+        sound = {filename = "__pyfusionenergygraphics__/sounds/prandium-lab.ogg", volume = 1.2},
+        idle_sound = {filename = "__pyfusionenergygraphics__/sounds/prandium-lab.ogg", volume = 0.8},
+        apparent_volume = 2.5
+    }
+}
