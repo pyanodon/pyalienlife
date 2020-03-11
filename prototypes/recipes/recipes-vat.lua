4ac28d0ae72206ace683bcdc1df665599c8cf7b3
pyanodon <smylodon@gmail.com>
2020-03-11 11:32:51 -0300

    vat
    Former-commit-id: bf045476d09a5d191544493b49cbdd860cfd8b9f
    Former-commit-id: bd661979c007fa55c7b36cbc08d381e0127b4861

--- prototypes/recipes/recipes-vat.lua
+++ prototypes/recipes/recipes-vat.lua
@@ -40,20 +40,20 @@ RECIPE {
     name = 'production-science-01',
     category = 'vat',
     enabled = false,
-    energy_required = 20,
+    energy_required = 60,
     ingredients = {
         {type = 'item', name = 'logistic-science-pack', amount = 10},
-        {type = 'item', name = 'chemical-science-pack', amount = 7},
-        {type = 'item', name = 'py-science-pack', amount = 5},
-        {type = 'item', name = 'brain', amount = 8},
+        {type = 'item', name = 'chemical-science-pack', amount = 10},
+        {type = 'item', name = 'py-science-pack', amount = 10},
+        {type = 'item', name = 'brain', amount = 15},
         {type = 'fluid', name = 'water', amount = 1000},
-        {type = 'fluid', name = 'fetal-serum', amount = 150},
-        {type = 'item', name = 'nexelit-plate', amount = 5},
+        {type = 'fluid', name = 'fetal-serum', amount = 250},
+        {type = 'item', name = 'nexelit-plate', amount = 15},
     },
     results = {
         {type = 'item', name = 'production-science-pack', amount = 1},
     },
-}:add_unlock("biotech-mk03"):add_ingredient({type = "item", name = "small-parts-02", amount = 10})
+}:add_unlock("biotech-mk03"):add_ingredient({type = "item", name = "small-parts-03", amount = 20})
 
 RECIPE {
     type = 'recipe',
