78923d11d941c3989bdc4292aea2032aa08453f6
pyanodon <smylodon@gmail.com>
2020-03-19 21:51:32 -0300

    fix creature chamber
    Former-commit-id: ad6da771766086f17ea63062245a888b500565e0
    Former-commit-id: 6c26de9ed5ad21d2c692bf10ca992ca7c5f6cdcd

--- prototypes/buildings/creature-chamber-mk01.lua
+++ prototypes/buildings/creature-chamber-mk01.lua
@@ -48,7 +48,7 @@ ENTITY {
     module_specification = {
         module_slots = 1
     },
-    allowed_effects = {"consumption", "speed", "productivity", "pollution"},
+    allowed_effects = {"consumption", "speed"},
     crafting_categories = {"creature-chamber"},
     crafting_speed = 1,
     energy_source = {
--- prototypes/buildings/creature-chamber-mk03.lua
+++ prototypes/buildings/creature-chamber-mk03.lua
@@ -45,7 +45,7 @@ ENTITY {
     module_specification = {
         module_slots = 3
     },
-    allowed_effects = {"consumption", "speed", "productivity", "pollution"},
+    allowed_effects = {"consumption", "speed"},
     crafting_categories = {"creature-chamber"},
     crafting_speed = 3,
     energy_source = {
