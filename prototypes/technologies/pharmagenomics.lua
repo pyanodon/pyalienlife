70bea0d956ddf4f2b3ee324683624a4ac9659474
pyanodon <smylodon@gmail.com>
2020-03-23 22:00:41 -0300

    phadaisus test
    Former-commit-id: 13c4deae0a233ab7add8730c7e7b2a03dfbf8dd7
    Former-commit-id: b06ec10ae74e3c09d559e52fd0638a9d2a92ce20

--- prototypes/technologies/pharmagenomics.lua
+++ prototypes/technologies/pharmagenomics.lua
@@ -12,6 +12,7 @@ TECHNOLOGY {
             {"automation-science-pack", 3},
             {"logistic-science-pack", 2},
             {'chemical-science-pack', 1},
+            {'production-science-pack', 1},
             {"py-science-pack", 2},
         },
         time = 50
