89194c3fba739f974ad2e433952cb49e3c1827ee
pyanodon <smylodon@gmail.com>
2020-05-06 18:56:44 -0300

    balance yeadols
    Former-commit-id: 5f77fe378dc5d08553157850967bd18afccf541d
    Former-commit-id: 49f8ff21ad2272967b925806f73bbfed4281e8a8

--- prototypes/technologies/yaedols.lua
+++ prototypes/technologies/yaedols.lua
@@ -4,14 +4,13 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/yaedols.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"mycology-mk03","genetic-design"},
+    prerequisites = {"mycology-mk02"},
     effects = {},
     unit = {
         count = 150,
         ingredients = {
             {"automation-science-pack", 4},
             {"logistic-science-pack", 1},
-            {'chemical-science-pack', 2},
         },
         time = 50
     }
