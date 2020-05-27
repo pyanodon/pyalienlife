a67c634af8ed35992a7abe515ada755daef99d6f
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-05-27 15:02:58 -0400

    adjustments for rocket silo
    Former-commit-id: 80bbbbbe807fcca9220fae0f6310485030eb22fc
    Former-commit-id: a38976a463851427e4277f718dedf92a20f30595

--- prototypes/technologies/bio-implants.lua
+++ prototypes/technologies/bio-implants.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/bio-implants.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"biotech-mk04"},
+    prerequisites = {"biotech-mk03"},
     effects = {},
     unit = {
         count = 250,
@@ -12,7 +12,7 @@ TECHNOLOGY {
             {"automation-science-pack", 3},
             {"logistic-science-pack", 3},
             {'chemical-science-pack', 2},
-            {'production-science-pack', 1},
+            --{'production-science-pack', 1},
             {"py-science-pack", 2},
         },
         time = 50
