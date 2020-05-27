6b2651662a5c0ad8920b51f1ef23dcd0bd2f59a3
pyanodon <smylodon@gmail.com>
2020-05-27 13:22:49 -0300

    lots of adjustments

--- prototypes/buildings/creature-chamber-mk02.lua
+++ prototypes/buildings/creature-chamber-mk02.lua
@@ -56,7 +56,7 @@ ENTITY {
         emissions_per_minute = 1,
     },
     energy_usage = "600kW",
-    ingredient_count = 10,
+    ingredient_count = 20,
     animation = {
         layers = {
             {
