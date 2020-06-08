fe51b1f9ec120a851d4111bdaac2b4d5857624b7
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-06-08 18:22:31 -0400

    add outpost 2

--- data.lua
+++ data.lua
@@ -299,6 +299,7 @@ require('prototypes/buildings/arqad-hive-mk04')
 require('prototypes/buildings/py-drive')
 
 require('prototypes/buildings/outpost')
+require('prototypes/buildings/outpost-mining')
 --require('prototypes/buildings/flyavan-outpost')
 --weapons/defenses
 --require('prototypes/weapons/turrets/gun-turret-mk01')
