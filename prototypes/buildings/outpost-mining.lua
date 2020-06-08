e37a5a7784ab1d95cb7edd7c625c835f6bc582da
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-06-08 18:22:31 -0400

    add outpost 2
    Former-commit-id: fe51b1f9ec120a851d4111bdaac2b4d5857624b7
    Former-commit-id: 82382790d4d0d9aab8740929c7727aed627af9ee

---
+++ prototypes/buildings/outpost-mining.lua
@@ -0,0 +1,62 @@
+	RECIPE {
+        type = "recipe",
+        name = 'outpost-mining',
+        enabled = false,
+        ingredients =
+			{
+                {"steel-plate", 50},
+                {"duralumin", 30},
+                {"electronic-circuit", 10},
+                {"lead-plate", 50},
+                {"titanium-plate", 100},
+			},
+        results = {
+            {'outpost-mining', 1}
+        }
+    }:add_unlock("auog"):add_ingredient({type = "item", name = "small-parts-01", amount = 100})
+
+    ITEM {
+        type = "item",
+        name = 'outpost-mining',
+        icon = "__pyalienlifegraphics__/graphics/icons/outpost.png",
+        icon_size = 64,
+        flags = {},
+        subgroup = "py-alienlife-buildings-others",
+        order = "a",
+        place_result = 'outpost-mining',
+        stack_size = 15
+    }
+
+	ENTITY {
+        type = "container",
+        name = 'outpost-mining',
+        icon = "__pyalienlifegraphics__/graphics/icons/outpost.png",
+        icon_size = 64,
+        flags = {"placeable-neutral", "placeable-player", "player-creation"},
+        minable = {mining_time = 2, result = 'outpost-mining'},
+        max_health = 350,
+        corpse = "big-remnants",
+        dying_explosion = "medium-explosion",
+        open_sound = {filename = "__base__/sound/machine-open.ogg", volume = 0.85},
+        close_sound = {filename = "__base__/sound/machine-close.ogg", volume = 0.75},
+        vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
+        resistances = {
+            {
+                type = "fire",
+                percent = 90
+            }
+        },
+        collision_box = {{-2.7, -2.7}, {2.7, 2.7}},
+        selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
+        drawing_box = {{-3.0, -3.0}, {3.0, 3.0}},
+        inventory_size = 800,
+        scale_info_icons = true,
+
+		picture = {
+            filename = "__pyalienlifegraphics2__/graphics/entity/outpost/off.png",
+            priority = "high",
+            width = 224,
+            height = 224,
+            shift = util.by_pixel(16, -16),
+        },
+    }
