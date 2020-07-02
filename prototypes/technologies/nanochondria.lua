ceb16c45a31f49f5c6bbeb34a355dea2200f1cd8
pyanodon <smylodon@gmail.com>
2020-07-02 09:27:03 -0300

    nano

--- prototypes/technologies/nanochondria.lua
+++ prototypes/technologies/nanochondria.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/nanochondria.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"bio-implants"},
+    prerequisites = {"biotech-mk04"},
     effects = {},
     unit = {
         count = 200,
