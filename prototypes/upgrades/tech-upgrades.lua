5350911c21d2b661e222c211f6c2c502d14a08ab
pyanodon <smylodon@gmail.com>
2020-07-21 12:46:35 -0300

    trying tech things
    Former-commit-id: 37f0f4d748e7abbd3e6afcd6c847264657aa3a8e
    Former-commit-id: a648c72b7f05a403f73624e0ca60bc84b968c6e7

--- prototypes/upgrades/tech-upgrades.lua
+++ prototypes/upgrades/tech-upgrades.lua
@@ -2,7 +2,7 @@ local FUN = require("prototypes/functions/functions")
 
 local tech_upgrade =
     {
-        test_1 = require("prototypes/upgrades/tech-upgrade-test")
+        test_1 = require("prototypes/upgrades/vrauks")
         --test_1 is a filler name for table readablity by fleshy meat bags. name it whatever and then set it to require the tech upgrade fills
         --each name needs to be unque to keep the files seperated
     }
