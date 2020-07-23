9a9d63f17cd193799a2105fc2ffaaf933a453534
pyanodon <smylodon@gmail.com>
2020-07-23 12:57:55 -0300

    preparing stuff
    Former-commit-id: d62753921790d234628adf355cf6304fae9dc6f2
    Former-commit-id: 771e81f0a51a0a8c76c53a49d0fa3665b8a80a5d

---
+++ prototypes/fluids/bio-oil.lua
@@ -0,0 +1,15 @@
+FLUID {
+    type = "fluid",
+    name = "bio-oil",
+    icon = "__pyalienlifegraphics__/graphics/icons/bio-oil.png",
+	icon_size = 64,
+    default_temperature = 10,
+    base_color = {r = 0.341, g = 0.482, b = 0.019},
+    flow_color = {r = 0.341, g = 0.482, b = 0.019},
+    max_temperature = 100,
+    gas_temperature = 15,
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    subgroup = "py-alienlife-fluids",
+    order = "f"
+}
