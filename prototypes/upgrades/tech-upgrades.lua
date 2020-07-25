37422219787f4185202cbbbc78464d13deac3ac1
pyanodon <smylodon@gmail.com>
2020-07-25 20:10:07 -0300

    techs

--- prototypes/upgrades/tech-upgrades.lua
+++ prototypes/upgrades/tech-upgrades.lua
@@ -3,6 +3,7 @@ local FUN = require("prototypes/functions/functions")
 local tech_upgrade =
     {
         test_1 = require("prototypes/upgrades/vrauks")
+        test_2 = require("prototypes/upgrades/slaughterhouse")
         --test_1 is a filler name for table readablity by fleshy meat bags. name it whatever and then set it to require the tech upgrade fills
         --each name needs to be unque to keep the files seperated
     }
