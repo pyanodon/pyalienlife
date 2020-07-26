a1536d2ffe37832d3e711529e9f8b52a915cae4d
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-07-25 22:30:22 -0400

    Update recipes-moss.lua
    Former-commit-id: 0795f9fd44769ec983059e39da8a4b06f621ed4b
    Former-commit-id: 8a521c85d872178c204c2e17a0020d88831497f4

--- prototypes/recipes/recipes-moss.lua
+++ prototypes/recipes/recipes-moss.lua
@@ -9,17 +9,20 @@ for _, rock in pairs(data.raw['simple-entity']) do
                 {type = 'item', name = 'stone', amount = rock.minable.count},
                 {type = 'item', name = 'moss', amount = 1, probability = 0.1}
             }
-    elseif rock.minable ~= nil and rock.minable.results ~= nil and rock.minable.results[1].item == 'stone' then
+    elseif rock.minable ~= nil and rock.minable.results ~= nil and rock.minable.results[1].name == 'stone' then
         table.insert(rock.minable.results, {type = 'item', name = 'moss', amount = 1, probability = 0.1})
     end
     if rock.loot ~= nil and rock.loot[1].item == 'stone' then
+        --[[
         rock.loot =
             {
                 {item = "stone", probability = 1, count_min = 9, count_max = 25},
                 {item = "moss", probability = 0.1, count = 1}
             }
+        ]]--
+        table.insert(rock.loot, {item = "moss", probability = 0.1, count = 1})
     end
-    --log(serpent.block(data.raw['simple-entity'][rock.name]))
+    log(serpent.block(data.raw['simple-entity'][rock.name]))
 end
 
 --Secondary Upgrade Recipes--
