0f986023ef08e9dd4ca78a7d870777ddf4608093
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-08-04 05:40:15 -0400

    techgrades back where i started

--- prototypes/upgrades/slaughterhouse.lua
+++ prototypes/upgrades/slaughterhouse.lua
@@ -1,130 +1,118 @@
 
---tech upgrade test example file
-
 local tech_upgrades =
     {
-        example_1 = -- can be whatever just used to identify this table
+        master_tech =
             {
-            technology = -- no touchy
+            name = "slaughterhouse-upgrade", -- this is the tech name. duh
+            icon = "__pyalienlifegraphics3__/graphics/technology/updates/u-slaugterhouse.png",
+            icon_size = 128,
+            order = "c-a",
+            prerequisites = {"rendering"},
+            unit = {
+                count = 500,
+                ingredients = {
+                    {"automation-science-pack", 1},
+                    {"logistic-science-pack", 1},
+                    {"chemical-science-pack", 1},
+                },
+                time = 45
+                }
+            },
+        sub_techs =
+            {
+            example_1 = -- can be whatever just used to identify this table
                 {
-                name = "laser-cutting", -- this is the tech name. duh
-                icon = "__pyalienlifegraphics3__/graphics/technology/laser-cutting.png", -- do i really need to spell it out for you
-                icon_size = 128,
-                order = "c-a",
-                prerequisites = {"rendering"},
-                unit = {
-                    count = 500,
-                    ingredients = {
-                        {"automation-science-pack", 1},
-                        {"logistic-science-pack", 1},
-                        {"chemical-science-pack", 1},
+                technology = -- no touchy
+                    {
+                    name = "laser-cutting", -- this is the tech name. duh
+                    icon = "__pyalienlifegraphics3__/graphics/technology/laser-cutting.png",
+                    icon_size = 128,
+                    order = "c-a",
                     },
-                    time = 45
+                    entities =
+                    {
+                        'slaughterhouse-mk01',
+                        'slaughterhouse-mk02',
+                        'slaughterhouse-mk03',
+                        'slaughterhouse-mk04',
+                    },-- the entities that should be effected by this tech upgrade. can be a single entity or a table of entities
+                upgrades = -- the effects the tech will have on the building. 1 = 100%
+                    {
+                        consumption = 0.8, --energy usage
+                        speed = 0.2, -- machine speed
+                        productivity = -0.2, -- productivity. and yes i know you`ll never use this but I`ll make sure it works anyway
+                        pollution = 0.3 -- pollution this machine will produce while running
+                    },
+                techs_to_lock = -- techs that should be locked and hidden if this tech is researched
+                    {
+                        'lard-machine',
+                        'mercy-killing',
                     }
                 },
+            example_2 =
+                {
+                technology =
+                    {
+                    name = "mercy-killing",
+                    icon = "__pyalienlifegraphics3__/graphics/technology/mercy-killing.png",
+                    icon_size = 128,
+                    order = "c-a",
+                    },
+                    entities =
+                    {
+                        'slaughterhouse-mk01',
+                        'slaughterhouse-mk02',
+                        'slaughterhouse-mk03',
+                        'slaughterhouse-mk04',
+                    },
+                upgrades =
+                    {
+                        consumption = 0.2,
+                        speed = -0.15,
+                        productivity = 0.2,
+                        pollution = 0.0
+                    },
+                techs_to_lock =
+                    {
+                        'lard-machine',
+                        'laser-cutting',
+                    },
+                is_upgrade = false, -- tell this its an upgrade of other techs
+                prerequisites = -- table of all techs that are replaced by this one.
+                    {
+                        --'example-1'
+                    }
+                },
+            example_3 =
+                {
+                technology =
+                    {
+                    name = "lard-machine",
+                    icon = "__pyalienlifegraphics3__/graphics/technology/lard-machine.png",
+                    icon_size = 128,
+                    order = "c-a",
+                    },
                 entities =
-                {
-                    'slaughterhouse-mk01',
-                    'slaughterhouse-mk02',
-                    'slaughterhouse-mk03',
-                    'slaughterhouse-mk04',
-                },-- the entities that should be effected by this tech upgrade. can be a single entity or a table of entities
-            upgrades = -- the effects the tech will have on the building. 1 = 100%
-                {
-                    consumption = 0.8, --energy usage
-                    speed = 0.2, -- machine speed
-                    productivity = -0.2, -- productivity. and yes i know you`ll never use this but I`ll make sure it works anyway
-                    pollution = 0.3 -- pollution this machine will produce while running
-                },
-            techs_to_lock = -- techs that should be locked and hidden if this tech is researched
-                {
-                    'lard-machine',
-                    'mercy-killing',
-                }
-            },
-        example_2 =
-            {
-            technology =
-                {
-                name = "mercy-killing",
-                icon = "__pyalienlifegraphics3__/graphics/technology/mercy-killing.png",
-                icon_size = 128,
-                order = "c-a",
-                prerequisites = {"rendering"},
-                unit = {
-                    count = 500,
-                    ingredients = {
-                        {"automation-science-pack", 1},
-                        {"logistic-science-pack", 1},
-                        {"chemical-science-pack", 1},
+                    {
+                        'slaughterhouse-mk01',
+                        'slaughterhouse-mk02',
+                        'slaughterhouse-mk03',
+                        'slaughterhouse-mk04',
                     },
-                    time = 45
+                upgrades =
+                    {
+                        consumption = 0,
+                        speed = -0.2,
+                        productivity = 0,
+                        pollution = -0.5
+                    },
+                techs_to_lock =
+                    {
+                        'laser-cutting',
+                        'mercy-killing'
                     }
                 },
-                entities =
-                {
-                    'slaughterhouse-mk01',
-                    'slaughterhouse-mk02',
-                    'slaughterhouse-mk03',
-                    'slaughterhouse-mk04',
-                },
-            upgrades =
-                {
-                    consumption = 0.2,
-                    speed = -0.15,
-                    productivity = 0.2,
-                    pollution = 0.0
-                },
-            techs_to_lock =
-                {
-                    'lard-machine',
-                    'laser-cutting',
-                },
-            is_upgrade = false, -- tell this its an upgrade of other techs
-            prerequisites = -- table of all techs that are replaced by this one.
-                {
-                    --'example-1'
-                }
-            },
-        example_3 =
-            {
-            technology =
-                {
-                name = "lard-machine",
-                icon = "__pyalienlifegraphics3__/graphics/technology/lard-machine.png",
-                icon_size = 128,
-                order = "c-a",
-                prerequisites = {"rendering"},
-                unit = {
-                    count = 500,
-                    ingredients = {
-                        {"automation-science-pack", 1},
-                        {"logistic-science-pack", 1},
-                        {"chemical-science-pack", 1},
-                    },
-                    time = 45
-                    }
-                },
-            entities =
-                {
-                    'slaughterhouse-mk01',
-                    'slaughterhouse-mk02',
-                    'slaughterhouse-mk03',
-                    'slaughterhouse-mk04',
-                },
-            upgrades =
-                {
-                    consumption = 0,
-                    speed = -0.2,
-                    productivity = 0,
-                    pollution = -0.5
-                },
-            techs_to_lock =
-                {
-                    'laser-cutting',
-                    'mercy-killing'
-                }
-            },
+            }
     }
 
 return(tech_upgrades)
--- prototypes/upgrades/tech-upgrades.lua
+++ prototypes/upgrades/tech-upgrades.lua
@@ -2,17 +2,17 @@ local FUN = require("prototypes/functions/functions")
 
 local tech_upgrade =
     {
-        test_1 = require("prototypes/upgrades/vrauks"),
-        --test_2 = require("prototypes/upgrades/slaughterhouse"),
-        --test_3 = require("prototypes/upgrades/auog"),
+        vrauk = require("prototypes/upgrades/vrauks"),
+        slaughterhouse = require("prototypes/upgrades/slaughterhouse"),
+        auog = require("prototypes/upgrades/auog"),
         --test_1 is a filler name for table readablity by fleshy meat bags. name it whatever and then set it to require the tech upgrade fills
         --each name needs to be unque to keep the files seperated
     }
 
 if data ~= nil then
-    log('hit')
+    --log('hit')
 FUN.tech_upgrade(tech_upgrade)
 else
-    log('hit')
+    --log('hit')
     return(tech_upgrade)
 end
