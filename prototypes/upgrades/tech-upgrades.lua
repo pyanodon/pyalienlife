823f63562be542e4ff201deadbcc8b782b77a6c9
pyanodon <smylodon@gmail.com>
2020-08-15 19:31:05 -0300

    atomizer

--- prototypes/upgrades/tech-upgrades.lua
+++ prototypes/upgrades/tech-upgrades.lua
@@ -10,6 +10,7 @@ local tech_upgrade =
         seaweed = require("prototypes/upgrades/seaweed"),
         sap = require("prototypes/upgrades/sap"),
         arthurian = require("prototypes/upgrades/arthurian"),
+        atomizer = require("prototypes/upgrades/atomizer"),
         --test_1 is a filler name for table readablity by fleshy meat bags. name it whatever and then set it to require the tech upgrade fills
         --each name needs to be unque to keep the files seperated
     }
