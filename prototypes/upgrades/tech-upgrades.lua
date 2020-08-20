0161c96bc08d24d0a2b1c432586da2f62b52426f
pyanodon <smylodon@gmail.com>
2020-08-19 21:10:02 -0300

    biopriting
    Former-commit-id: d74c5cd04bc8dcdcd691ca5c7f12a638b81965a9
    Former-commit-id: ac9849d249dc90bdb8dab1d06d92af51d287024c

--- prototypes/upgrades/tech-upgrades.lua
+++ prototypes/upgrades/tech-upgrades.lua
@@ -13,6 +13,7 @@ local tech_upgrade =
         arthurian = require("prototypes/upgrades/arthurian"),
         atomizer = require("prototypes/upgrades/atomizer"),
         bhoddos = require("prototypes/upgrades/bhoddos"),
+        bioprinting = require("prototypes/upgrades/bioprinting"),
         --test_1 is a filler name for table readablity by fleshy meat bags. name it whatever and then set it to require the tech upgrade fills
         --each name needs to be unque to keep the files seperated
     }
