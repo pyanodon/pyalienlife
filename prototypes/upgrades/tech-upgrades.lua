59b6ecd5938146c07fd161d5e38f175e9fdaf538
pyanodon <smylodon@gmail.com>
2020-08-26 21:45:42 -0300

    bioreactor
    Former-commit-id: ad532862d144c78d8b7169d7d405d926adca9cde
    Former-commit-id: b4415db076f9819d72e68ac25c7f57e4bf410254

--- prototypes/upgrades/tech-upgrades.lua
+++ prototypes/upgrades/tech-upgrades.lua
@@ -15,6 +15,7 @@ local tech_upgrade =
         bhoddos = require("prototypes/upgrades/bhoddos"),
         bioprinting = require("prototypes/upgrades/bioprinting"),
         biofactory = require("prototypes/upgrades/biofactory"),
+        bioreactor = require("prototypes/upgrades/bioreactor"),
         --test_1 is a filler name for table readablity by fleshy meat bags. name it whatever and then set it to require the tech upgrade fills
         --each name needs to be unque to keep the files seperated
     }
