3e27ee4cfbf511d3654e8ea46734f767e190581a
pyanodon <smylodon@gmail.com>
2021-04-05 08:39:24 -0300

    pesticide

--- prototypes/recipes/recipes.lua
+++ prototypes/recipes/recipes.lua
@@ -2259,6 +2259,7 @@ RECIPE {
         {type = 'item', name = 'aluminium-plate', amount = 2},
         {type = 'fluid', name = 'ethanol', amount = 50},
         {type = 'fluid', name = 'acidgas', amount = 100},
+        {type = 'fluid', name = 'phosphorous-acid', amount = 100},
     },
     results = {
         {type = 'fluid', name = 'ethyl-mercaptan', amount = 200},
