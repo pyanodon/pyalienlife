85eb368e19171f1d9ef0dc2efc079811d6930737
ShadowGlass0 <gkecskem@gmail.com>
2021-10-28 13:58:45 +1100

    #47 Remove phenol from pre-pesticide-01 if no PyHT

--- prototypes/updates/pyhightech-updates.lua
+++ prototypes/updates/pyhightech-updates.lua
@@ -337,6 +337,7 @@ RECIPE('bose-einstein-superfluid'):add_ingredient({type = "fluid", name = "deute
 RECIPE('green-sic'):add_ingredient({type = 'item', name = 'graphite', amount = 3}):remove_unlock('bioprocessing'):add_unlock('basic-electronics')
 RECIPE('sic'):remove_unlock('bioprocessing'):add_unlock('basic-electronics')
 RECIPE('pesticide-mk02'):add_ingredient({type = 'fluid', name = 'carbon-sulfide', amount = 50})
+RECIPE('pre-pesticide-01'):add_ingredient({type = 'item', name = 'phenol', amount = 10})
 
 RECIPE('simik-food-01'):add_ingredient({type = "item", name = "cadaveric-arum", amount = 2})
 fun.add_ingredient('simik-food-01', {name = "cadaveric-arum", amount = 2})
