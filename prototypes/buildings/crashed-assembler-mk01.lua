5c2a694c3975601299bd94299d134df5eb5d7098
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-01-22 10:12:18 +0000

    Fix output fluidbox height on crashed assembler
    -1 -> 1

--- prototypes/buildings/crashed-assembler-mk01.lua
+++ prototypes/buildings/crashed-assembler-mk01.lua
@@ -134,7 +134,7 @@ ENTITY{
             pipe_picture = DATA.Pipes.pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
             pipe_covers = DATA.Pipes.covers(false, true, true, true),
             base_area = 10,
-            base_level = -1,
+            base_level = 1,
             pipe_connections = {{type = "output", position = {-1, 2}}}
         },
       off_when_no_fluid_recipe = true
