b08ef266aeffe57d00b285976028315b44edc048
ShadowGlass0 <gkecskem@gmail.com>
2022-04-11 00:42:54 +1000

    Standardize samples (#98)

--- prototypes/recipes/dingrits/recipes-dingrits.lua
+++ prototypes/recipes/dingrits/recipes-dingrits.lua
@@ -45,7 +45,7 @@ RECIPE {
     name = 'earth-wolf-sample',
     category = 'data-array',
     enabled = false,
-    energy_required = 30,
+    energy_required = 300,
     ingredients = {
         {type = 'item', name = 'earth-generic-sample', amount = 1},
         {type = 'item', name = 'chemical-science-pack', amount = 20},
