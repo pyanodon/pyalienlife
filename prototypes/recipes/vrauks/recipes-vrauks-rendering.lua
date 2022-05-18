6e6f3d9b722bfea6aeeb0d1668ceb067bbdc75de
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-05-17 22:56:34 -0400

    Update recipes-vrauks-rendering.lua

--- prototypes/recipes/vrauks/recipes-vrauks-rendering.lua
+++ prototypes/recipes/vrauks/recipes-vrauks-rendering.lua
@@ -5,15 +5,15 @@ RECIPE {
     enabled = false,
     energy_required = 10,
     ingredients = {
-        { name = 'caged-vrauks', amount = 1 }
+        {type = "item", name = 'caged-vrauks', amount = 1 }
     },
     results = {
-        { name = 'meat', amount = 2 },
-        { name = 'chitin', amount = 1 },
-        { name = 'guts', amount = 2 },
-        { name = 'formic-acid', amount = 200 },
-        { name = 'brain', amount = 1 },
-        { name = 'cage', amount = 1 }
+        {type = "item", name = 'meat', amount = 2 },
+        {type = "item", name = 'chitin', amount = 1 },
+        {type = "item", name = 'guts', amount = 2 },
+        {type = "fluid", name = 'formic-acid', amount = 200 },
+        {type = "item", name = 'brain', amount = 1 },
+        {type = "item", name = 'cage', amount = 1 }
     },
     icon = '__pyalienlifegraphics__/graphics/icons/rendering-vrauks.png',
     icon_size = 64,
