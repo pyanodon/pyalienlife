2f7b06e939535a91374115ba495f8250661bc2ab
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-07-17 01:12:14 -0400

    removed duplicated moss mk02 recipes
    resolves pyanodon/pybugreports#54

--- prototypes/recipes/moss/recipes-moss-modules.lua
+++ prototypes/recipes/moss/recipes-moss-modules.lua
@@ -1,7 +1,7 @@
 
 --Secondary Upgrade Recipes--
 --SUR--
---WIP--
+--DONE--
 
 --mk02--
 --DONE--
@@ -23,25 +23,8 @@ RECIPE {
     },
   }:add_unlock("moss-mk02"):add_ingredient({type = "item", name = "urea", amount = 1})
 
-RECIPE {
-    type = 'recipe',
-    name = 'moss-mk02',
-    category = 'moss',
-    enabled = true,
-    energy_required = 25,
-    ingredients = {
-        {type = 'fluid', name = 'dirty-water-light', amount = 200},
-        {type = 'item', name = 'sodium-alginate', amount = 1},
-        {type = 'item', name = 'moss', amount = 5},
-        {type = 'item', name = 'wood', amount = 5},
-    },
-    results = {
-        {type = 'item', name = 'moss-mk02', amount = 1},
-    },
-  }:add_unlock("moss-mk02"):add_ingredient({type = "item", name = "urea", amount = 1})
-
 --mk03--
---WIP--
+--DONE--
 
   RECIPE {
     type = 'recipe',
@@ -50,18 +33,18 @@ RECIPE {
     enabled = true,
     energy_required = 25,
     ingredients = {
-        {type = 'fluid', name = 'dirty-water-light', amount = 200},
-        {type = 'item', name = 'sodium-alginate', amount = 1},
+        {type = 'fluid', name = 'dirty-water-heavy', amount = 200},
+        {type = 'item', name = 'fertilizer', amount = 10},
         {type = 'item', name = 'moss', amount = 5},
-        {type = 'item', name = 'wood', amount = 5},
+        {type = 'item', name = 'tuuphra-seeds', amount = 5},
     },
     results = {
-        {type = 'item', name = 'moss-mk02', amount = 1},
+        {type = 'item', name = 'moss-mk03', amount = 1},
     },
-  }:add_unlock("moss-mk03"):add_ingredient({type = "item", name = "urea", amount = 1})
+  }:add_unlock("moss-mk03")
 
 --mk04--
---WIP--
+--DONE--
 
   RECIPE {
     type = 'recipe',
@@ -70,12 +53,12 @@ RECIPE {
     enabled = true,
     energy_required = 25,
     ingredients = {
-        {type = 'fluid', name = 'dirty-water-light', amount = 200},
-        {type = 'item', name = 'sodium-alginate', amount = 1},
-        {type = 'item', name = 'moss', amount = 5},
-        {type = 'item', name = 'wood', amount = 5},
+        {type = 'fluid', name = 'gta', amount = 200},
+        {type = 'item', name = 'immunosupressants', amount = 5},
+        {type = 'item', name = 'moss-mk03', amount = 5},
+        {type = 'item', name = 'bhoddos', amount = 5},
     },
     results = {
-        {type = 'item', name = 'moss-mk02', amount = 1},
+        {type = 'item', name = 'moss-mk04', amount = 1},
     },
-  }:add_unlock("moss-mk04"):add_ingredient({type = "item", name = "urea", amount = 1})
+  }:add_unlock("moss-mk04")
