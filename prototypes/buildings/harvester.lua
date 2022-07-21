73ecc507f30541decc705b9ce7c73c6ee04a7e1b
ShadowGlass0 <gkecskem@gmail.com>
2022-07-22 03:11:18 +1000

    Harvester: Fix vector_to_place_result (#152)
    Relates to pyanodon/pybugreport#9

--- prototypes/buildings/harvester.lua
+++ prototypes/buildings/harvester.lua
@@ -70,7 +70,7 @@ ENTITY {
     energy_usage = "600kW",
     mining_power = 2,
     resource_searching_radius = 4.49,
-    vector_to_place_result = {0, -4.05},
+    vector_to_place_result = {0, -3.75},
     radius_visualisation_picture = {
         filename = "__base__/graphics/entity/electric-mining-drill/electric-mining-drill-radius-visualization.png",
         width = 4,
