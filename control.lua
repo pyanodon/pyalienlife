400d04f2fe7ac42ffb2245d8563d64f06db538bd
kingarthur91 <arthurkoy_durbin@yahoo.com>
2023-01-05 03:22:18 -0500

    version and changelog

--- control.lua
+++ control.lua
@@ -212,6 +212,7 @@ script.on_init(function()
     if remote.interfaces['freeplay'] then
         local ship_items = remote.call('freeplay', 'get_ship_items')
         ship_items['iron-chest'] = 5
+        created_items["firearm-magazine"] = 500
         remote.call('freeplay', 'set_ship_items', ship_items)
     end
 end)
