4b582d6aa2fa096d0ea27581159a9f058b570b52
Sm^HpJk4 <zacharypicco@gmail.com>
2023-01-10 17:35:20 -0600

    Fix crash with migrations

--- migrations/2.0.7.lua
+++ migrations/2.0.7.lua
@@ -3,7 +3,7 @@ if global.farms then
 	global.farms = nil
 	global.indexed_farm_list = nil
 	rendering.clear('pyalienlife')
-	require '__pyalienlife__/scripts/farming.lua'
+	require '__pyalienlife__/scripts/farming/farming.lua'
 	Farming.events.on_init()
 	for _, surface in pairs(game.surfaces) do
 		for _, entity in pairs(surface.find_entities_filtered{type = 'assembling-machine'}) do
