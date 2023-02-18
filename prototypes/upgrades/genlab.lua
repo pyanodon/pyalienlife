b2276feb6f290e836017d6614c9e3296f6d8d577
Sm^HpJk4 <zacharypicco@gmail.com>
2023-02-18 09:22:53 -0600

    genlab improvement

--- prototypes/upgrades/genlab.lua
+++ prototypes/upgrades/genlab.lua
@@ -6,18 +6,26 @@ if data then
     local adam = table.deepcopy(data.raw.recipe['adam42-gen'])
     adam.name = 'adam42-gen-laser'
     FUN.add_ingredient(adam, {name = 'personal-laser-defense-equipment', amount = 1, type = 'item'})
-    FUN.add_result(adam, {name = 'personal-laser-defense-equipment', amount = 1, type = 'item', probability = 0.9, catalyst_amount = 1})
-    FUN.multiply_result_amount(adam, 'adam42-gen', 2)
+    FUN.add_result(adam, {name = 'personal-laser-defense-equipment', amount = 1, type = 'item', probability = 0.75, catalyst_amount = 1})
+    FUN.remove_result(adam, 'adam42-gen')
+    FUN.add_result(adam, {name = 'adam42-gen', type = 'item', amount_min = 1, amount_max = 2})
     adam.energy_required = adam.energy_required * 4
     adam.main_product = 'adam42-gen'
-    data:extend{adam}
+
+    local pks = table.deepcopy(data.raw.recipe['enzyme-pks'])
+    pks.name = 'enzyme-pks-nickel-carbonyl'
+    FUN.add_ingredient(pks, {name = 'nickel-carbonyl', amount = 200, type = 'fluid'})
+    FUN.multiply_result_amount(pks, 'enzyme-pks', 2)
+    pks.energy_required = pks.energy_required * 2
+    pks.main_product = 'enzyme-pks'
 
     local hmas = table.deepcopy(data.raw.recipe['hmas'])
     hmas.name = 'hmas-pvp'
     FUN.add_ingredient(hmas, {name = pyAE and 'pvp' or 'stone', amount = 2, type = 'item'})
     FUN.multiply_result_amount(hmas, 'hmas', 2.5)
-    hmas.energy_required = hmas.energy_required * 2
-    data:extend{hmas}
+    hmas.energy_required = hmas.energy_required * 3
+
+    data:extend{adam, pks, hmas}
 end
 
 return {
@@ -64,7 +72,7 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {speed = -0.8, productivity = 0.05, type = 'module-effects'}
+                {old = 'enzyme-pks', new = 'enzyme-pks-nickel-carbonyl', type = 'recipe-replacement'}
             }
         },
         {
--- prototypes/upgrades/scrondrix.lua
+++ prototypes/upgrades/scrondrix.lua
@@ -32,6 +32,7 @@ if data then
     local experimental = table.deepcopy(data.raw.recipe['Caged scrondrix 9'])
     experimental.name = 'scrondrix-experimental-treatment'
     FUN.add_ingredient(experimental, {name = 'arthurian-codex', amount = 1, type = 'item'})
+    FUN.multiply_result_amount(experimental, 'pineal-gland', 0.5)
     experimental.results = {
         {name = 'bones', amount = 1, type = 'item', probability = 0.6},
         {name = 'cage', amount = 1, type = 'item', probability = 0.6},
