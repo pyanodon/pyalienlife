19ea8773bfd9a0205bae6b91d4d9e48dccd5cf99
Sm^HpJk4 <zacharypicco@gmail.com>
2023-02-21 21:32:23 -0600

    phagnot

--- prototypes/upgrades/phagnot.lua
+++ prototypes/upgrades/phagnot.lua
@@ -1,3 +1,55 @@
+local FUN = require '__pycoalprocessing__/prototypes/functions/functions'
+
+if data then
+    data:extend{
+        {
+            type = 'recipe',
+            name = 'gas-bladder-to-deuterium',
+            enabled = false,
+            energy_required = 10,
+            category = 'gas-refinery',
+            ingredients = {{'gas-bladder', 1}},
+            results = {{type = 'fluid', name = 'deuterium', amount = 5}}
+        },
+        {
+            type = 'recipe',
+            name = 'gas-bladder-to-dry-gas-stream',
+            enabled = false,
+            energy_required = 10,
+            category = 'gas-refinery',
+            ingredients = {{'gas-bladder', 1}},
+            results = {{type = 'fluid', name = 'dry-gas-stream', amount = 5}}
+        }
+    }
+
+    for _, recipe in pairs({
+        table.deepcopy(data.raw.recipe['phagnot-cub-1']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-2']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-3']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-4']),
+    }) do
+        recipe.name = recipe.name .. '-fast'
+        recipe.energy_required = math.ceil(recipe.energy_required * 0.7)
+        data:extend{recipe}
+    end
+
+    for _, recipe in pairs({
+        table.deepcopy(data.raw.recipe['phagnot-1']),
+        table.deepcopy(data.raw.recipe['phagnot-2']),
+        table.deepcopy(data.raw.recipe['phagnot-3']),
+        table.deepcopy(data.raw.recipe['phagnot-4']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-1']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-2']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-3']),
+        table.deepcopy(data.raw.recipe['phagnot-cub-4']),
+    }) do
+        recipe.name = recipe.name .. '-kicalk'
+        FUN.add_ingredient(recipe, {name = 'kicalk', amount = 1, type = 'item'})
+        FUN.remove_ingredient(recipe, 'raw-fiber')
+        data:extend{recipe}
+    end
+end
+
 return {
     affected_entities = { -- the entities that should be effected by this tech upgrade
         'phagnot-corral-mk01',
@@ -28,7 +80,8 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {consumption = 0.0, speed = 0.15, productivity = -0.12, type = 'module-effects'}
+                {recipe = 'gas-bladder-to-deuterium', type = 'unlock-recipe'},
+                {recipe = 'gas-bladder-to-dry-gas-stream', type = 'unlock-recipe'},
             },
         },
         {
@@ -37,7 +90,10 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {consumption = -0.1, speed = 0, productivity = -0.1, type = 'module-effects'}
+            {old = 'phagnot-cub-1', new = 'phagnot-cub-1-fast', type = 'recipe-replacement'},
+            {old = 'phagnot-cub-2', new = 'phagnot-cub-2-fast', type = 'recipe-replacement'},
+            {old = 'phagnot-cub-3', new = 'phagnot-cub-3-fast', type = 'recipe-replacement'},
+            {old = 'phagnot-cub-4', new = 'phagnot-cub-4-fast', type = 'recipe-replacement'},
             }
         },
         {
@@ -46,7 +102,14 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {consumption = 0.25, speed = 0, productivity = 0.2, type = 'module-effects'}
+                {old = 'phagnot-1', new = 'phagnot-1-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-2', new = 'phagnot-2-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-3', new = 'phagnot-3-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-4', new = 'phagnot-4-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-cub-1', new = 'phagnot-cub-1-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-cub-2', new = 'phagnot-cub-2-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-cub-3', new = 'phagnot-cub-3-kicalk', type = 'recipe-replacement'},
+                {old = 'phagnot-cub-4', new = 'phagnot-cub-4-kicalk', type = 'recipe-replacement'},
             }
         }
     },
