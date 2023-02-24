63e1a1cb01bed1aa49f7564b080f3a0418bbbc07
Sm^HpJk4 <zacharypicco@gmail.com>
2023-02-23 18:22:23 -0600

    ulric

--- prototypes/upgrades/ulric.lua
+++ prototypes/upgrades/ulric.lua
@@ -1,3 +1,73 @@
+local FUN = require '__pycoalprocessing__/prototypes/functions/functions'
+
+if data then
+    data:extend{
+        {
+            type = 'item',
+            name = 'saddle',
+            stack_size = 10,
+            subgroup = 'py-alienlife-items',
+            icon = '__pyalienlifegraphics__/graphics/icons/saddle.png',
+            icon_size = 64
+        },
+        {
+            type = 'recipe',
+            name = 'saddle',
+            energy_required = 2,
+            category = 'crafting-with-fluid',
+            enabled = false,
+            ingredients = {
+                {name = 'skin', amount = 4, type = 'item'},
+                {name = 'niobium-plate', amount = 1, type = 'item'},
+                {name = 'flue-gas', amount = 250, type = 'fluid'},
+            },
+            result = 'saddle'
+        },
+        {
+            type = 'recipe',
+            name = 'noooo dont turn that horse into glue',
+            energy_required = 8,
+            enabled = false,
+            category = 'impact-crusher',
+            ingredients = {{name = 'caged-ulric', amount = 1, type = 'item'}},
+            results = {
+                {name = 'epoxy', amount = 2, type = 'item'},
+                {name = 'grade-4-ti', amount = 1, type = 'item'},
+            },
+            main_product = 'epoxy',
+            subgroup = 'py-rawores-titanium'
+        }
+    }
+
+    for _, recipe in pairs({
+        table.deepcopy(data.raw.recipe['ulric-cub-1']),
+        table.deepcopy(data.raw.recipe['ulric-cub-2']),
+        table.deepcopy(data.raw.recipe['ulric-cub-3']),
+        table.deepcopy(data.raw.recipe['ulric-cub-4']),
+    }) do
+        recipe.name = recipe.name .. '-saddle'
+        for _, result in pairs(recipe.results) do
+            if result.name == 'ulric-cub' then
+                FUN.add_ingredient(recipe, {name = 'saddle', amount = result.amount, type = 'item'})
+                break
+            end
+        end
+        data:extend{recipe}
+    end
+
+    for i, recipe in pairs({
+        table.deepcopy(data.raw.recipe['ulric-1']),
+        table.deepcopy(data.raw.recipe['ulric-2']),
+        table.deepcopy(data.raw.recipe['ulric-3']),
+        table.deepcopy(data.raw.recipe['ulric-4']),
+    }) do
+        recipe.name = recipe.name .. '-manure'
+        FUN.add_result(recipe, {name = 'manure', amount = 2, type = 'item'})
+        if i ~= 1 then FUN.add_result(recipe, {name = 'bedding', amount_max = 1, type = 'item', amount_min = 0}) end
+        data:extend{recipe}
+    end
+end
+
 return {
     affected_entities = { -- the entities that should be effected by this tech upgrade
         'ulric-corral-mk01',
@@ -10,13 +80,13 @@ return {
         icon = '__pyalienlifegraphics3__/graphics/technology/updates/u-ulric.png',
         icon_size = 128,
         order = 'c-a',
-        prerequisites = {'ulric-mk02'},
+        prerequisites = {'ulric', 'niobium'},
         unit = {
             count = 500,
             ingredients = {
                 {'automation-science-pack', 1},
+                {'py-science-pack-1', 1},
                 {'logistic-science-pack', 1},
-                {'chemical-science-pack', 1},
             },
             time = 45
         }
@@ -28,7 +98,7 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {consumption = 0.0, speed = 0.15, productivity = -0.12, type = 'module-effects'}
+                {recipe = 'noooo dont turn that horse into glue', type = 'unlock-recipe'}
             },
         },
         {
@@ -37,7 +107,12 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {consumption = -0.1, speed = 0, productivity = -0.1, type = 'module-effects'}
+                {speed = 0.3, productivity = 0.15, type = 'module-effects'},
+                {recipe = 'saddle', type = 'unlock-recipe'},
+                {old = 'ulric-cub-1', new = 'ulric-cub-1-saddle', type = 'recipe-replacement'},
+                {old = 'ulric-cub-2', new = 'ulric-cub-2-saddle', type = 'recipe-replacement'},
+                {old = 'ulric-cub-3', new = 'ulric-cub-3-saddle', type = 'recipe-replacement'},
+                {old = 'ulric-cub-4', new = 'ulric-cub-4-saddle', type = 'recipe-replacement'},
             }
         },
         {
@@ -46,7 +121,10 @@ return {
             icon_size = 128,
             order = 'c-a',
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'lock-recipe', 'recipe-replacement'
-                {consumption = 0.25, speed = 0, productivity = 0.2, type = 'module-effects'}
+                {old = 'ulric-1', new = 'ulric-1-manure', type = 'recipe-replacement'},
+                {old = 'ulric-2', new = 'ulric-2-manure', type = 'recipe-replacement'},
+                {old = 'ulric-3', new = 'ulric-3-manure', type = 'recipe-replacement'},
+                {old = 'ulric-4', new = 'ulric-4-manure', type = 'recipe-replacement'},
             }
         }
     },
