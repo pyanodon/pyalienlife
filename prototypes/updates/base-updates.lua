4292efc5069573ac60ca49106f54547c3bd17db9
kingarthur91 <arthurkoy_durbin@yahoo.com>
2023-04-22 06:36:30 -0400

    Update base-updates.lua

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -31,10 +31,10 @@ TECHNOLOGY("personal-laser-defense-equipment"):add_pack("py-science-pack-3")
 TECHNOLOGY("energy-shield-equipment"):add_pack("py-science-pack-2")
 
 TECHNOLOGY('fluid-handling'):remove_pack('logistic-science-pack')
-TECHNOLOGY('automation-2'):remove_pack('logistic-science-pack'):remove_prereq('logistic-science-pack'):remove_prereq("electronics")
+TECHNOLOGY('automation-2'):add_pack("py-science-pack-1"):remove_pack('logistic-science-pack'):remove_prereq('logistic-science-pack'):remove_prereq("electronics")
+TECHNOLOGY("automation-3"):remove_pack("chemical-science-pack"):remove_pack("production-science-pack")add_pack("py-science-pack-2"):set_fields{prerequisites = {}}
 TECHNOLOGY("fast-inserter"):add_pack("py-science-pack-1")
 TECHNOLOGY("heavy-armor"):add_pack("py-science-pack-1")
-TECHNOLOGY("automation-2"):add_pack("py-science-pack-1")
 TECHNOLOGY("logistic-science-pack"):add_pack("py-science-pack-1")
 TECHNOLOGY("automated-rail-transportation"):add_pack("py-science-pack-1")
 TECHNOLOGY("rail-signals"):add_pack("py-science-pack-1")
