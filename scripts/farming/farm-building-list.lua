fb15897d15863dca63935abf0c315037ed4c7146
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-11-26 13:49:53 -0700

    fix the root cause of pyanodon/pybugreports#345
    Needs a global reference to the local, I guess.

--- scripts/farming/farm-building-list.lua
+++ scripts/farming/farm-building-list.lua
@@ -41,8 +41,6 @@ local farm_buildings = {
 	['fawogae-plantation'] = 'fungi',
 	['navens-culture'] = 'fungi',
 	['yaedols-culture'] = 'fungi',
-	['zungror-lair'] = 'animal',
-	['numal-reef'] = 'animal'
 }
 
 return farm_buildings
