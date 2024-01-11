e012e92fdab41f4c4dea7d683589424b2b1e159c
notnotmelon <zacharypicco@gmail.com>
2024-01-10 18:24:35 -0600

    revert my research turd change. putting it too early leads to softlocks

--- prototypes/upgrades/research.lua
+++ prototypes/upgrades/research.lua
@@ -42,6 +42,7 @@ return {
             ingredients = {
                 {'automation-science-pack', 1},
                 {'logistic-science-pack', 1},
+                {'chemical-science-pack', 1},
             },
             time = 45
         }
