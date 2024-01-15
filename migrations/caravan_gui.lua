4b1ff1f5f31abde6d2d6885fdc8f9293c122c94b
Thorsten Goetzke <th.goetzke@gmx.net>
2024-01-15 20:56:19 +0100

    fixed the migration

--- migrations/caravan_gui.lua
+++ migrations/caravan_gui.lua
@@ -1,5 +1,8 @@
-local gui = game.player.gui.relative.caravan_gui
-if gui then
-    gui.destroy()
-end --it lives in a flow now
+for _,player in pairs(game.players) do
+    local gui = player.gui.relative.caravan_gui
+    if gui then
+        gui.destroy()
+    end --it lives in a flow now
+end
+
 
