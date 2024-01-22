9df16790fa3b560b964f89a914c8b4d6a3aeb8d1
notnotmelon <zacharypicco@gmail.com>
2024-01-21 21:05:22 -0600

    Apply pyanodon code formatting standard

--- scripts/caravan/caravan-global-gui.lua
+++ scripts/caravan/caravan-global-gui.lua
@@ -1,31 +1,31 @@
 require 'caravan-gui-shared'
 local Table = require('__stdlib__/stdlib/utils/table')
-local close_button_name='click_close_global_gui'
+local close_button_name = 'click_close_global_gui'
 
 local function add_titlebar(gui, caption, close_button_name)
-    local titlebar = gui.add{type = "flow"}
+    local titlebar = gui.add{type = 'flow'}
     titlebar.drag_target = gui
     titlebar.add{
-        type = "label",
-        style = "frame_title",
+        type = 'label',
+        style = 'frame_title',
         caption = caption,
-        ignored_by_interaction = true,
+        ignored_by_interaction = true
     }
     local filler = titlebar.add{
-        type = "empty-widget",
-        style = "draggable_space",
-        ignored_by_interaction = true,
+        type = 'empty-widget',
+        style = 'draggable_space',
+        ignored_by_interaction = true
     }
     filler.style.height = 24
     filler.style.horizontally_stretchable = true
     titlebar.add{
-        type = "sprite-button",
+        type = 'sprite-button',
         name = close_button_name,
-        style = "frame_action_button",
-        sprite = "utility/close_white",
-        hovered_sprite = "utility/close_black",
-        clicked_sprite = "utility/close_black",
-        tooltip = {"gui.close-instruction"},
+        style = 'frame_action_button',
+        sprite = 'utility/close_white',
+        hovered_sprite = 'utility/close_black',
+        clicked_sprite = 'utility/close_black',
+        tooltip = {'gui.close-instruction'}
     }
 end
 
@@ -34,12 +34,12 @@ function Caravan.has_any_caravan_at_all()
 end
 
 local function instantiate_main_frame(player)
-    local main_frame = player.gui.screen.add { type = 'frame', name = 'caravan_gui_global',  direction = 'vertical' }
-    add_titlebar(main_frame,{ 'caravan-global-gui.caption' },'click_close_global_gui')
+    local main_frame = player.gui.screen.add{type = 'frame', name = 'caravan_gui_global', direction = 'vertical'}
+    add_titlebar(main_frame, {'caravan-global-gui.caption'}, 'click_close_global_gui')
     main_frame.style.width = 336
     main_frame.style.minimal_height = 710
     main_frame.auto_center = true
-    player.opened=main_frame
+    player.opened = main_frame
     return main_frame
 end
 
@@ -48,23 +48,24 @@ Caravan.events.on_open_global_gui = function(event)
     if Caravan.close_global_gui(event) then
         return
     end
-    local main_frame=instantiate_main_frame(player)
-    local content_frame = main_frame.add { type = 'frame', direction = 'vertical',
-                                           style = 'inside_shallow_frame_with_padding' }
+    local main_frame = instantiate_main_frame(player)
+    local content_frame = main_frame.add{
+        type = 'frame',
+        direction = 'vertical',
+        style = 'inside_shallow_frame_with_padding'
+    }
     content_frame.style.vertically_stretchable = true
-    local content_flow = content_frame.add { type = 'flow', direction = 'vertical' }
+    local content_flow = content_frame.add{type = 'flow', direction = 'vertical'}
     content_flow.style.vertical_spacing = 8
-    content_flow.style.margin = { -4, 0, -4, 0 }
+    content_flow.style.margin = {-4, 0, -4, 0}
     content_flow.style.vertical_align = 'center'
 
-    local scroll_pane = content_flow.add {
-        type = 'scroll-pane',
-    }
+    local scroll_pane = content_flow.add{type = 'scroll-pane'}
     if not Caravan.has_any_caravan_at_all() then
-        scroll_pane.add{type= 'label', caption={'caravan-global-gui.empty'}}
+        scroll_pane.add{type = 'label', caption = {'caravan-global-gui.empty'}}
         return
     end
-    local table = scroll_pane.add {
+    local table = scroll_pane.add{
         type = 'table',
         column_count = 2
     }
@@ -87,12 +88,9 @@ end
 
 local function is_our_event(event)
     local player = game.get_player(event.player_index)
-    if not event.element then
-        return
-    end
-    return event.element.name==close_button_name or  event.element == player.gui.screen.caravan_gui_global
+    if not event.element then return end
+    return event.element.name == close_button_name or event.element == player.gui.screen.caravan_gui_global
 end
-
 
 Caravan.events.on_close_global_gui = function(event)
     if is_our_event(event) then
@@ -100,7 +98,7 @@ Caravan.events.on_close_global_gui = function(event)
     end
 end
 
-Caravan.close_global_gui= function(event)
+Caravan.close_global_gui = function(event)
     local player = game.get_player(event.player_index)
     if player.gui.screen.caravan_gui_global then
         player.gui.screen.caravan_gui_global.destroy()
