9858b6f6fd7ed4469dd10531c9a44610ef98fb31
notnotmelon <zacharypicco@gmail.com>
2024-02-02 13:45:12 -0600

    Added a migration to clean any remaining bugged bioport graphics

---
+++ migrations/2.1.22.lua
@@ -0,0 +1,8 @@
+for _, surface in pairs(game.surfaces) do
+    for _, arm in pairs(surface.find_entities_filtered{name = 'bioport-floor-animation'}) do
+        local port = surface.find_entity('bioport', arm.position)
+        if not (port and port.position.x == arm.position.x and port.position.y == arm.position.y) then
+            arm.destroy()
+        end
+    end
+end
