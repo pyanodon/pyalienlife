f1aa817a4dd5631ce2bf13a6c4b284265e163b24
notnotmelon <zacharypicco@gmail.com>
2024-06-03 13:18:17 -0500

    The smart farm now properly cleans up any autoplaced tilled dirt tiles when the entity is mined.

--- scripts/smart-farm/smart-farm.lua
+++ scripts/smart-farm/smart-farm.lua
@@ -20,24 +20,43 @@ for _, filename in pairs(farm_filenames) do
 	farms[farm.seed] = farm
 end
 
+Smart_Farm.events.on_init = function()
+	global.smart_farm_landfill_data = global.smart_farm_landfill_data or {}
+end
+
+local function get_fence_positions(entity)
+	local position = entity.position
+	position.y = position.y - 15
+	local fence_positions = {}
+	for x = -12, 12 do
+		table.insert(fence_positions, {position.x + x, position.y + 13})
+		table.insert(fence_positions, {position.x + x, position.y - 13})
+	end
+	for y = -13, 13 do
+		table.insert(fence_positions, {position.x + 13, position.y + y})
+		table.insert(fence_positions, {position.x - 13, position.y + y})
+	end
+	return fence_positions
+end
+
+local function get_landfill_positions(entity)
+	local position = entity.position
+	position.y = position.y - 15
+	local landfill_positions = {}
+	for x = -12, 12 do
+		for y = -12, 12 do
+			table.insert(landfill_positions, {position.x + x, position.y + y})
+		end
+	end
+	return landfill_positions
+end
+
 Smart_Farm.events.on_built = function(event)
 	local entity = event.created_entity or event.entity
     if entity.name ~= 'mega-farm' then return end
 	local surface = entity.surface
 
-	local position = entity.position
-	position.y = position.y - 15
-	local positions = {}
-	for x = -13, 13 do
-		table.insert(positions, {position.x + x, position.y + 13})
-		table.insert(positions, {position.x + x, position.y - 13})
-	end
-	for y = -13, 13 do
-		table.insert(positions, {position.x + 13, position.y + y})
-		table.insert(positions, {position.x - 13, position.y + y})
-	end
-
-	for _, position in pairs(positions) do
+	for _, position in pairs(get_fence_positions(entity)) do
 		if not surface.entity_prototype_collides('wood-fence', position, false) then
 			surface.create_entity{
 				name = 'wood-fence',
@@ -48,13 +67,21 @@ Smart_Farm.events.on_built = function(event)
 	end
 
 	local landfill_tiles = {}
-	for x = -12, 12 do
-		for y = -12, 12 do
-			local position = {position.x + x, position.y + y}
-			if not surface.get_tile(position).collides_with('water-tile') then
-				table.insert(landfill_tiles, {name = 'landfill', position = position})
-			end
+	for _, position in pairs(get_landfill_positions(entity)) do
+		local x, y = position[1], position[2]
+
+		local previous_tile = surface.get_tile(position)
+		if not previous_tile.valid or previous_tile.collides_with('water-tile') then goto continue end
+		table.insert(landfill_tiles, {name = 'landfill', position = position})
+
+		global.smart_farm_landfill_data[x] = global.smart_farm_landfill_data[x] or {}
+		if global.smart_farm_landfill_data[x][y] then
+			global.smart_farm_landfill_data[x][y].depth = global.smart_farm_landfill_data[x][y].depth + 1
+		else
+			global.smart_farm_landfill_data[x][y] = {depth = 1, name = previous_tile.name}
 		end
+
+		::continue::
 	end
 	surface.set_tiles(landfill_tiles)
 end
@@ -64,24 +91,35 @@ Smart_Farm.events.on_destroyed = function(event)
 	if entity.name ~= 'mega-farm' then return end
 	local surface = entity.surface
 
-	local position = entity.position
-	position.y = position.y - 15
-	local positions = {}
-	for x = -13, 13 do
-		table.insert(positions, {position.x + x, position.y + 13})
-		table.insert(positions, {position.x + x, position.y - 13})
-	end
-	for y = -13, 13 do
-		table.insert(positions, {position.x + 13, position.y + y})
-		table.insert(positions, {position.x - 13, position.y + y})
-	end
-
-	for _, position in pairs(positions) do
+	for _, position in pairs(get_fence_positions(entity)) do
 		local fence = surface.find_entity('wood-fence', position)
 		if fence and fence.force_index == entity.force_index then
 			fence.destroy()
 		end
 	end
+
+	local tiles_to_reset = {}
+	for _, position in pairs(get_landfill_positions(entity)) do
+		local x, y = position[1], position[2]
+
+		if not global.smart_farm_landfill_data[x] or not global.smart_farm_landfill_data[x][y] then goto continue end
+
+		local data = global.smart_farm_landfill_data[x][y]
+		data.depth = data.depth - 1
+
+		if data.depth > 0 then goto continue end
+		table.insert(tiles_to_reset, {name = data.name, position = position})
+
+		global.smart_farm_landfill_data[x][y] = nil
+		if table_size(global.smart_farm_landfill_data[x]) == 0 then
+			global.smart_farm_landfill_data[x] = nil
+		end
+
+		::continue::
+	end
+
+	if table_size(tiles_to_reset) == 0 then return end
+	surface.set_tiles(tiles_to_reset, true, false, false, true)
 end
 
 Smart_Farm.events.on_rocket_launched = function(event)
