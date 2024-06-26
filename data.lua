161bfd9e25e132294ee17b1d507fa38e4c0fd410
notnotmelon <zacharypicco@gmail.com>
2024-06-26 12:13:45 -0500

    Fix crash on load

--- data.lua
+++ data.lua
@@ -352,7 +352,7 @@ require 'prototypes/buildings/simik-den-mk01'
 require 'prototypes/buildings/simik-den-mk02'
 require 'prototypes/buildings/simik-den-mk03'
 require 'prototypes/buildings/simik-den-mk04'
-require 'pyalienlife.prototypes.buildings.rc'
+require 'prototypes/buildings/rc'
 
 require 'prototypes/buildings/hidden-beacon'
 
