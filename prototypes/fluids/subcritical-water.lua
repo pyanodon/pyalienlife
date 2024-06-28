639addb14301e2ea7358f64130ca2899494f0568
notnotmelon <zacharypicco@gmail.com>
2024-06-27 20:54:52 -0500

    apply linting to subcritial-water.lua

--- prototypes/fluids/subcritical-water.lua
+++ prototypes/fluids/subcritical-water.lua
@@ -6,13 +6,13 @@ RECIPE {
     energy_required = 30,
     ingredients = {
         {type = 'fluid', name = 'pressured-water', amount = 200},
-        {type = 'item', name = 'biomass', amount = 30},
+        {type = 'item',  name = 'biomass',         amount = 30},
     },
     results = {
         {type = 'fluid', name = 'subcritical-water', amount = 50},
     },
-    main_product = "subcritical-water"
-}:add_unlock("cottongut-science-mk01")
+    main_product = 'subcritical-water'
+}:add_unlock('cottongut-science-mk01')
 
 RECIPE {
     type = 'recipe',
@@ -22,26 +22,26 @@ RECIPE {
     energy_required = 10,
     ingredients = {
         {type = 'fluid', name = 'pressured-water', amount = 200},
-        {type = 'item', name = 'fuelrod-mk01', amount = 1},
+        {type = 'item',  name = 'fuelrod-mk01',    amount = 1},
     },
     results = {
         {type = 'fluid', name = 'subcritical-water', amount = 200},
     },
-    main_product = "subcritical-water"
-}:add_unlock("biotech-mk03")
+    main_product = 'subcritical-water'
+}:add_unlock('biotech-mk03')
 
 FLUID {
-    type = "fluid",
-    name = "subcritical-water",
-    icon = "__pyalienlifegraphics__/graphics/icons/subcritical-water.png",
-	icon_size = 64,
+    type = 'fluid',
+    name = 'subcritical-water',
+    icon = '__pyalienlifegraphics__/graphics/icons/subcritical-water.png',
+    icon_size = 64,
     default_temperature = 10,
-    base_color = {r=0, g=0.34, b=0.6},
+    base_color = {r = 0, g = 0.34, b = 0.6},
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 100,
     gas_temperature = 15,
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
-    subgroup = "py-alienlife-fluids",
-    order = "f"
+    subgroup = 'py-alienlife-fluids',
+    order = 'f'
 }
