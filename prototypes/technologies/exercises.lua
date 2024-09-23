487e725774e0511a8a416dc3578b52da62d40af9
kingarthur91 <arthurkoy_durbin@yahoo.com>
2024-09-22 23:22:04 -0400

    remove disabled prereq

--- prototypes/technologies/exercises.lua
+++ prototypes/technologies/exercises.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/exercises.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"genetic-design"},
+    prerequisites = {},
     effects = {},
     unit = {
         count = 400,
