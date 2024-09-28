49ecec60509a310b2b1f818c53463d324561f40e
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-28 02:18:05 +0200

    fix miner graphics

--- prototypes/buildings/fluid-drill-mk01.lua
+++ prototypes/buildings/fluid-drill-mk01.lua
@@ -99,7 +99,7 @@ ENTITY {
     circuit_connector_sprites = circuit_connector_definitions["fluid-drill-mkxx"].sprites,
     circuit_wire_max_distance = default_circuit_wire_max_distance,
     graphics_set = {
-        animations = {
+        animation = {
             layers = {
                 {
                     filename = "__pyalienlifegraphics2__/graphics/entity/fluid-mining-drill/raw.png",
