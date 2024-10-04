13429f8724da7c531daeec08e2cab59c11d09d1f
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-10-04 21:32:08 +0200

    fix mega-farms

--- prototypes/buildings/mega-farm.lua
+++ prototypes/buildings/mega-farm.lua
@@ -88,7 +88,7 @@ ENTITY {
           pipe_covers = py.pipe_covers(false, true, true, true),
           pipe_picture = py.pipe_pictures("assembling-machine-3", nil, {-0.00, -0.95}, nil, nil),
           volume = 1000,
-          pipe_connections = {{flow_direction = "input", position = {3.5, 0.0}, direction = defines.direction.east}},
+          pipe_connections = {{flow_direction = "input", position = {3.0, 0.0}, direction = defines.direction.east}},
           priority = "extra-high"
       },
       {
@@ -96,7 +96,7 @@ ENTITY {
         pipe_covers = py.pipe_covers(false, true, true, true),
         pipe_picture = py.pipe_pictures("assembling-machine-3", nil, {-0.00, -0.95}, nil, nil),
         volume = 1000,
-        pipe_connections = {{flow_direction = "input", position = {-3.5, 0.0}, direction = defines.direction.west}},
+        pipe_connections = {{flow_direction = "input", position = {-3.0, 0.0}, direction = defines.direction.west}},
         priority = "extra-high"
     },
     },
