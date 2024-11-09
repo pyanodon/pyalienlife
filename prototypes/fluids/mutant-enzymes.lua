4994a9f4d321980a47cbe8fb80c407e183a55583
notnotmelon <zacharypicco@gmail.com>
2024-11-09 12:38:18 -0600

    Removed all code related to `FluidPrototype.pressure_to_speed_ratio`

--- prototypes/fluids/mutant-enzymes.lua
+++ prototypes/fluids/mutant-enzymes.lua
@@ -25,7 +25,6 @@ FLUID {
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 100,
     gas_temperature = 15,
-    pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
     subgroup = "py-alienlife-fluids",
     order = "f"
