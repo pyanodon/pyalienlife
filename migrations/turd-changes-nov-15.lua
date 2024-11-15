86ad20d296d1b61019f23f41b671716a6c8c7ee7
notnotmelon <zacharypicco@gmail.com>
2024-11-15 13:02:49 -0600

    Mukmoux turd rebalance

---
+++ migrations/turd-changes-nov-15.lua
@@ -0,0 +1,5 @@
+local turd_migrate = require "__pyalienlife__/scripts/turd/turd-migration"
+
+turd_migrate("mukmoux-upgrade", "zero-cross")
+turd_migrate("mukmoux-upgrade", "think-collar")
+turd_migrate("mukmoux-upgrade", "bip")
