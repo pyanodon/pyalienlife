edc5e32cf05bf9d6b915dcfd3b3a04cb7e5b6104
Alex ten Brink <alextenbrink@gmail.com>
2024-12-16 22:51:55 +0100

    Redid bioreactor turd entirely.
    TODO:
    - balancing
    - locale
    - images?

---
+++ migrations/3.0.32.lua
@@ -0,0 +1,5 @@
+local turd_migrate = require "__pyalienlife__/scripts/turd/turd-migration"
+
+turd_migrate("bioreactor-upgrade", "aerators")
+turd_migrate("bioreactor-upgrade", "baffles")
+turd_migrate("bioreactor-upgrade", "jacket")
--- prototypes/upgrades/bioreactor.lua
+++ prototypes/upgrades/bioreactor.lua
@@ -1,95 +1,102 @@
 if data and not yafc_turd_integration then
     data:extend {
         {
-            type = "item",
-            stack_size = 50,
-            name = "baffles",
-            icon = "__pyalienlifegraphics__/graphics/icons/baffles.png",
-            icon_size = 64,
-            subgroup = "py-alternativeenergy-items"
+            type = "fuel-category",
+            name = "bio-reactor-1"
         },
         {
-            type = "recipe",
-            name = "baffles",
-            result = "baffles",
-            ingredients = {
-                {type = "item",  name = "rpc-mesh",       amount = 8},
-                {type = "item",  name = "cobalt-sulfate", amount = 20},
-                {type = "item",  name = "hardener",       amount = 2},
-                {type = "fluid", name = "fluorine-gas",   amount = 100},
-            },
-            enabled = false,
-            category = "nmf",
-            energy_required = 10
+            type = "fuel-category",
+            name = "bio-reactor-2"
         },
         {
-            type = "recipe",
-            name = "liquid-manure-oxygen",
-            ingredients = {
-                {type = "item",  name = "manure",          amount = 12},
-                {type = "item",  name = "soil",            amount = 8},
-                {type = "fluid", name = "water",           amount = 900},
-                {type = "fluid", name = "oxygen",          amount = 100},
-                {type = "fluid", name = "manure-bacteria", amount = 30},
-            },
-            results = {
-                {type = "fluid", name = "liquid-manure",     amount = 350},
-                {type = "fluid", name = "black-liquor",      amount = 350},
-                {type = "item",  name = "re-precipitate-02", amount = 3},
-            },
-            enabled = false,
-            category = "bio-reactor",
-            energy_required = 60,
-            main_product = "liquid-manure"
+            type = "fuel-category",
+            name = "bio-reactor-3"
         },
         {
-            type = "recipe",
-            name = "jacket-bio-ore",
-            enabled = false,
-            category = "bio-reactor",
-            energy_required = 450,
-            ingredients = {
-                {type = "item",  name = "nanozymes",          amount = 1},
-                {type = "item",  name = "magnetic-beads",     amount = 2},
-                {type = "fluid", name = "hydrogen",           amount = 250},
-                {type = "fluid", name = "benzene",            amount = 100},
-                {type = "item",  name = "advanced-substrate", amount = 2},
-            },
-            results = {
-                {type = "item", name = "bio-ore", amount = 250},
-            },
-            main_product = "bio-ore"
-        },
-        {
-            type = "recipe",
-            name = "jacket-bio-oil",
-            enabled = false,
-            category = "bio-reactor",
-            energy_required = 450,
-            ingredients = {
-                {type = "item",  name = "nanozymes",          amount = 1},
-                {type = "item",  name = "magnetic-beads",     amount = 2},
-                {type = "fluid", name = "hydrogen",           amount = 250},
-                {type = "fluid", name = "aromatics",          amount = 100},
-                {type = "item",  name = "advanced-substrate", amount = 4},
-            },
-            results = {
-                {type = "fluid", name = "bio-oil", amount = 4000},
-            },
-            main_product = "bio-oil"
+            type = "recipe-category",
+            name = "advanced-bio-reactor"
         }
     }
-    
-    for i, recipe in pairs {
-        RECIPE("bio-reactor-mk01"):copy(),
-        RECIPE("bio-reactor-mk02"):copy(),
-        RECIPE("bio-reactor-mk03"):copy(),
-        RECIPE("bio-reactor-mk04"):copy(),
-    } do
-        recipe.name = recipe.name .. "-with-baffles"
-        recipe:add_ingredient {name = "baffles", amount = 10 * i, type = "item"}
-        data:extend {recipe}
+    data.raw.item["xeno-egg"].fuel_value = "1MJ"
+    data.raw.item["xeno-egg"].fuel_category = "bio-reactor-1"
+    data.raw.item["high-flux-core"].fuel_value = "1MJ"
+    data.raw.item["high-flux-core"].fuel_category = "bio-reactor-2"
+    data.raw.item["strorix-unknown-sample"].fuel_value = "1MJ"
+    data.raw.item["strorix-unknown-sample"].fuel_category = "bio-reactor-3"
+
+    for j = 1, 3 do
+        for i = 1, 4 do
+            local name = "bio-reactor-mk0" .. i
+            local entity = table.deepcopy(data.raw["assembling-machine"][name])
+            entity.name = "advanced-" .. name .. "-turd" .. j
+            if i ~= 4 then
+                entity.next_upgrade = "advanced-bio-reactor-mk0" .. i + 1 .. "-turd" .. j
+            end
+            entity.crafting_speed = entity.crafting_speed * 2
+            entity.module_slots = entity.module_slots * 2
+            entity.crafting_categories = { "advanced-bio-reactor", "bio-reactor" }
+
+            entity.localised_name = {"entity-name." .. name}
+            entity.placeable_by = {item = name, count = 1}
+            entity.localised_description = entity.localised_description or {"entity-description." .. name}
+            table.insert(entity.flags, "not-in-made-in")
+            entity.energy_source = {
+                type = "burner",
+                fuel_categories = {"bio-reactor-"..j},
+                effectivity = 1,
+                fuel_inventory_size = 1,
+                burnt_inventory_size = 1,
+                emissions_per_minute = {
+                    pollution = 0.06
+                },
+            }
+            entity.energy_usage = i .. "kW"
+            data:extend {entity}
+        end
     end
+
+    data:extend {
+        {
+            type = "recipe",
+            name = "path-1-advanced-recipe",
+            ingredients = {
+                {type = "item",  name = "stone-wall",     amount = 1},
+                {type = "item",  name = "poison-capsule", amount = 5},
+                {type = "item",  name = "caged-xeno",     amount = 1},
+            },
+            results = {
+                {type = "fluid", name = "hydrogen-peroxide", amount = 75},
+                {type = "fluid", name = "anthraquinone",     amount = 125},
+                {type = "item",  name = "cage",              amount = 1, probability = 0.5},
+            },
+            main_product = "hydrogen-peroxide",
+            enabled = false,
+            category = "advanced-bio-reactor",
+            energy_required = 10,
+        },
+        {
+            type = "recipe",
+            name = "path-2-advanced-recipe",
+            ingredients = {
+                {type = "item",  name = "fawogae-substrate",  amount = 1},
+                {type = "item",  name = "magnetic-beads",     amount = 1},
+                {type = "item",  name = "nanozymes",          amount = 1},
+                {type = "item",  name = "quartz-tube",        amount = 1},
+                {type = "item",  name = "nano-cellulose",     amount = 1},
+                {type = "fluid", name = "clean-organic-pulp", amount = 100},
+            },
+            result = "nems",
+            enabled = false,
+            category = "advanced-bio-reactor",
+            energy_required = 10,
+        },
+    }
+
+    local path3 = RECIPE("chitosan"):copy()
+    path3.name = "path-3-advanced-recipe"
+    path3.ingredients[3] = {type = "fluid", name = "blood",   amount = 50}
+    path3.category = "advanced-bio-reactor"
+    data:extend{path3}
 end
 
 return {
@@ -129,7 +136,11 @@ return {
             icon_size = 128,
             order = "c-a",
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'recipe-replacement', 'machine-replacement'
-                {recipe = "liquid-manure-oxygen", type = "unlock-recipe"}
+                {type = "machine-replacement", old = "bio-reactor-mk01", new = "advanced-bio-reactor-mk01-turd1"},
+                {type = "machine-replacement", old = "bio-reactor-mk02", new = "advanced-bio-reactor-mk02-turd1"},
+                {type = "machine-replacement", old = "bio-reactor-mk03", new = "advanced-bio-reactor-mk03-turd1"},
+                {type = "machine-replacement", old = "bio-reactor-mk04", new = "advanced-bio-reactor-mk04-turd1"},
+                {type = "unlock-recipe",       recipe = "path-1-advanced-recipe"},
             },
         },
         {
@@ -138,12 +149,11 @@ return {
             icon_size = 128,
             order = "c-a",
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'recipe-replacement', 'machine-replacement'
-                {consumption = -0.25,      speed = 0.25,                          productivity = 0.04,        type = "module-effects"},
-                {recipe = "baffles",       type = "unlock-recipe"},
-                {old = "bio-reactor-mk01", new = "bio-reactor-mk01-with-baffles", type = "recipe-replacement"},
-                {old = "bio-reactor-mk02", new = "bio-reactor-mk02-with-baffles", type = "recipe-replacement"},
-                {old = "bio-reactor-mk03", new = "bio-reactor-mk03-with-baffles", type = "recipe-replacement"},
-                {old = "bio-reactor-mk04", new = "bio-reactor-mk04-with-baffles", type = "recipe-replacement"},
+                {type = "machine-replacement", old = "bio-reactor-mk01", new = "advanced-bio-reactor-mk01-turd2"},
+                {type = "machine-replacement", old = "bio-reactor-mk02", new = "advanced-bio-reactor-mk02-turd2"},
+                {type = "machine-replacement", old = "bio-reactor-mk03", new = "advanced-bio-reactor-mk03-turd2"},
+                {type = "machine-replacement", old = "bio-reactor-mk04", new = "advanced-bio-reactor-mk04-turd2"},
+                {type = "unlock-recipe",       recipe = "path-2-advanced-recipe"},
             }
         },
         {
@@ -152,8 +162,11 @@ return {
             icon_size = 128,
             order = "c-a",
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'recipe-replacement', 'machine-replacement'
-                {recipe = "jacket-bio-ore", type = "unlock-recipe"},
-                {recipe = "jacket-bio-oil", type = "unlock-recipe"},
+                {type = "machine-replacement", old = "bio-reactor-mk01", new = "advanced-bio-reactor-mk01-turd3"},
+                {type = "machine-replacement", old = "bio-reactor-mk02", new = "advanced-bio-reactor-mk02-turd3"},
+                {type = "machine-replacement", old = "bio-reactor-mk03", new = "advanced-bio-reactor-mk03-turd3"},
+                {type = "machine-replacement", old = "bio-reactor-mk04", new = "advanced-bio-reactor-mk04-turd3"},
+                {type = "unlock-recipe",       recipe = "path-3-advanced-recipe"},
             }
         }
     },
