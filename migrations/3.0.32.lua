0bc0c29948c26133a9cf0e2201881a32dc600e5f
Vraleth <arajuk@gmail.com>
2024-12-27 17:57:54 +0100

    Redid scrondrix path 3. No more free brains for you guys. Now affects diamond production. Redid slaughterhouse TURD entirely.

--- migrations/3.0.32.lua
+++ migrations/3.0.32.lua
@@ -3,3 +3,7 @@ local turd_migrate = require "__pyalienlife__/scripts/turd/turd-migration"
 turd_migrate("bioreactor-upgrade", "aerators")
 turd_migrate("bioreactor-upgrade", "baffles")
 turd_migrate("bioreactor-upgrade", "jacket")
+turd_migrate("slaughterhouse-upgrade", "laser-cutting")
+turd_migrate("slaughterhouse-upgrade", "mercy-killing")
+turd_migrate("slaughterhouse-upgrade", "lard-machine")
+turd_migrate("scrondrix-upgrade", "neuron")
--- prototypes/upgrades/slaughterhouse.lua
+++ prototypes/upgrades/slaughterhouse.lua
@@ -1,6 +1,8 @@
 local pyAE = (data and mods.pyalternativeenergy) or (script and script.active_mods.pyalternativeenergy)
 local pySE = (data and mods.pystellarexpedition) or (script and script.active_mods.pystellarexpedition)
 
+
+
 local rendering_recipes = {
     "full-render-zipir",
     "full-render-dhilmoss",
@@ -35,52 +37,94 @@ if pySE then
     --table.insert(rendering_recipes, 'full-render-riga')
     --table.insert(rendering_recipes, 'full-render-tuls')
 end
-local effects = {laser = {}, music = {}, lard = {}}
 
 if data and not yafc_turd_integration then
-    local things_to_add = {
-        laser = {ingredients = {{type = "item", name = "laser-turret", amount = 1}}, results = {{type = "item", name = "laser-turret", amount = 1, probability = 0.999}}},
-        music = {ingredients = {{type = "item", name = "programmable-speaker", amount = 1}}, results = {{type = "item", name = "programmable-speaker", amount = 1, probability = 0.99}}},
-        lard = {ingredients = {{type = "fluid", name = "grease", amount = 3}}, results = {}}
+
+    -- PATH 1 -> Recipes with multiple creature giving the sum of both + some bonus
+    local g_mixData = {
+        { name = "slaughterhouse-path1-brains", main = "brain", body1 = "full-render-arthurian", body2 = "full-render-cottongut", ratio = 1.5},
+        { name = "slaughterhouse-path1-lard", main = "mukmoux-fat", body1 = "full-render-mukmoux", body2 = "full-render-trit", ratio = 2},
+        { name = "slaughterhouse-path1-blood", main = "blood", body1 = "full-render-ulrics", body2 = "full-render-auogs", ratio = 2},
+        { name = "slaughterhouse-path1-meat", main = "meat", body1 = "full-render-kor", body2 = "full-render-auogs", ratio = 2},
+        { name = "slaughterhouse-path1-chitin", main = "chitin", body1 = "full-render-dhilmoss", body2 = "full-render-vrauks", ratio = 2},
+        { name = "slaughterhouse-path1-arthropod-blood", main = "arthropod-blood", body1 = "full-render-xyhiphoe", body2 = "full-render-zipir", ratio = 2},
+        { name = "slaughterhouse-path1-guts", main = "guts", body1 = "full-render-xyhiphoe", body2 = "full-render-fish", ratio = 2},
+        { name = "slaughterhouse-path1-skin", main = "skin", body1 = "full-render-cottongut", body2 = "full-render-mukmoux", ratio = 2},
+        { name = "slaughterhouse-path1-bones", main = "bones", body1 = "full-render-phagnots", body2 = "full-render-scrondrixs", ratio = 2},
     }
 
-    local products_to_buff = {
-        laser = {"blood", "mukmoux-fat", "sulfuric-acid", "bones", "brain"},
-        music = {"skin", "carapace", "pelt", "chitin", "shell", "fish-oil", "photophore"},
-        lard = {"meat", "arthropod-blood", "guts", "formic-acid", "bonemeal", "tendon", "keratin"}
-    }
-
-    for _, path in pairs {"laser", "music", "lard"} do
-        for _, recipe_name in pairs(rendering_recipes) do
-            local recipe = RECIPE(recipe_name):copy(function(r) return r.name .. "-" .. path end)
-            recipe.localised_name = recipe.localised_name or {"recipe-name." .. recipe_name}
-            for _, ingredient in pairs(things_to_add[path].ingredients) do
-                recipe:add_ingredient(ingredient)
-            end
-            for _, result in pairs(things_to_add[path].results) do
-                recipe:add_result(result)
-            end
-            for _, product in pairs(products_to_buff[path]) do
-                if product == "brain" then
-                    for _, result in pairs(recipe.results) do
-                        if (result[1] or result.name) == "brain" then
-                            recipe:add_result {name = "brain", amount = 1, type = "item", probability = 0.05}
-                            break
-                        end
+    -- Melding recipes together. The py lib can do it for ingredients but not for results, hence this
+    for _,d in ipairs(g_mixData) do
+        local r2 = RECIPE(d.body2):copy()
+        local recipe = RECIPE(d.body1):copy(function(r) return d.name end)
+        recipe.localised_name = recipe.localised_name or {"recipe-name." .. d.name}
+ 
+        for _, ingredient in pairs(r2.ingredients) do
+            recipe:add_ingredient(ingredient)
+        end
+        for _, result in pairs(r2.results) do
+            local done = false;
+            for _, existing in pairs(recipe.results) do
+                if existing.name == result.name and existing.type == result.type then
+                    if existing.amount and result.amount then
+                        existing.amount = existing.amount + result.amount
+                        existing.ignored_by_productivity = (existing.ignored_by_productivity or 0) + (result.ignored_by_productivity or 0)
+                        done = true
+                        break
                     end
-                else
-                    recipe:multiply_result_amount(product, 2)
                 end
             end
-            data:extend {recipe}
-            table.insert(effects[path], {old = recipe_name, new = recipe.name, type = "recipe-replacement"})
+            if false == done then recipe:add_result(result) end
         end
+        for _, result in pairs(recipe.results) do
+            if (result[1] or result.name) == d.main then
+                recipe:multiply_result_amount(d.main, d.ratio)
+                break
+            end
+        end
+        data:extend {recipe}
     end
-elseif script then
-    for _, path in pairs {"laser", "music", "lard"} do
-        for _, recipe_name in pairs(rendering_recipes) do
-            table.insert(effects[path], {old = recipe_name, new = recipe_name .. "-" .. path, type = "recipe-replacement"})
+
+    -- PATH 2 -> Violent slaughtering damages the building when processing. Building runs 10x faster. Make sure you have constant supply of repair packs otherwise the machine will break completely.
+
+    for i = 1, 4 do
+        local name = "slaughterhouse-mk0" .. i
+        local entity = table.deepcopy(data.raw["assembling-machine"][name])
+        entity.localised_name = {"entity-name." .. name}
+        entity.localised_description = entity.localised_description or {"entity-description." .. name}
+        entity.name = "violent-" .. name
+        if i ~= 4 then
+            entity.next_upgrade = "violent-slaughterhouse-mk0" .. (i + 1)
         end
+        entity.crafting_speed = entity.crafting_speed * 10
+        entity.placeable_by = {item = name, count = 1}
+        entity.max_health = 400*i
+        entity.hidden = true
+        entity.hidden_in_factoripedia = true
+        entity.production_health_effect = {
+            producing = (-0.0005*i),
+            not_producing = 0,
+        }
+        data:extend {entity}
+    end
+
+    -- PATH 3 -> Allow vatbrains to effect slaughterhouses. Reduces processing speed of all slaughterhouses. Big Net negative if not using vatbrains, but with them ....
+
+    for i = 1, 4 do
+        local name = "slaughterhouse-mk0" .. i
+        local entity = table.deepcopy(data.raw["assembling-machine"][name])
+        entity.localised_name = {"entity-name." .. name}
+        entity.localised_description = entity.localised_description or {"entity-description." .. name}
+        entity.name = "smart-" .. name
+        if i ~= 4 then
+            entity.next_upgrade = "smart-slaughterhouse-mk0" .. (i + 1)
+        end
+        entity.placeable_by = {item = name, count = 1}
+        entity.allowed_module_categories = {"vatbrain", "speed"}
+        entity.crafting_speed = entity.crafting_speed * 0.25
+        entity.hidden = true
+        entity.hidden_in_factoripedia = true
+        data:extend {entity}
     end
 end
 
@@ -108,27 +152,48 @@ return {
             time = 45
         }
     },
+
     sub_techs = {
         {
             name = "laser-cutting",
             icon = "__pyalienlifegraphics3__/graphics/technology/laser-cutting.png",
             icon_size = 128,
             order = "c-a",
-            effects = effects.laser,
+            effects = {
+                {recipe = "slaughterhouse-path1-brains", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-lard", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-blood", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-meat", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-chitin", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-arthropod-blood", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-guts", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-skin", type = "unlock-recipe"},
+                {recipe = "slaughterhouse-path1-bones", type = "unlock-recipe"}
+            }
         },
         {
             name = "mercy-killing",
             icon = "__pyalienlifegraphics3__/graphics/technology/mercy-killing.png",
             icon_size = 128,
             order = "c-a",
-            effects = effects.music
+            effects = {
+                {type = "machine-replacement", old = "slaughterhouse-mk01", new = "violent-slaughterhouse-mk01"},
+                {type = "machine-replacement", old = "slaughterhouse-mk02", new = "violent-slaughterhouse-mk02"},
+                {type = "machine-replacement", old = "slaughterhouse-mk03", new = "violent-slaughterhouse-mk03"},
+                {type = "machine-replacement", old = "slaughterhouse-mk04", new = "violent-slaughterhouse-mk04"},
+            }
         },
         {
             name = "lard-machine",
             icon = "__pyalienlifegraphics3__/graphics/technology/lard-machine.png",
             icon_size = 128,
             order = "c-a",
-            effects = effects.lard
+            effects = {
+                {type = "machine-replacement", old = "slaughterhouse-mk01", new = "smart-slaughterhouse-mk01"},
+                {type = "machine-replacement", old = "slaughterhouse-mk02", new = "smart-slaughterhouse-mk02"},
+                {type = "machine-replacement", old = "slaughterhouse-mk03", new = "smart-slaughterhouse-mk03"},
+                {type = "machine-replacement", old = "slaughterhouse-mk04", new = "smart-slaughterhouse-mk04"},
+            }
         }
     }
 }
