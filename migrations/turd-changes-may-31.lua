d4ec06862983f39642e93598075edc26810ac1b1
Zachary Picco <zacharypicco@gmail.com>
2025-05-31 20:33:06 -0500

    Buffed all three paths of simik turd path 2.

---
+++ migrations/turd-changes-may-31.lua
@@ -0,0 +1,5 @@
+local turd_migrate = require "__pyalienlife__/scripts/turd/turd-migration"
+
+turd_migrate("simik-digestion-mk02", "simik-coal")
+turd_migrate("simik-digestion-mk02", "simik-tin")
+turd_migrate("simik-digestion-mk02", "simik-aluminium")
