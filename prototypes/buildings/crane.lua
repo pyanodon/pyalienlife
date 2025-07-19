5a78d7db3433d7ca61ec7837a5834c501d541abb
Alyssa Doubrovsky <adoubrovsky@gmail.com>
2025-07-18 17:14:18 -0700

    Update crane.lua
    Added ingredients for crane mk1 added crane mk2 and mk3

--- prototypes/buildings/crane.lua
+++ prototypes/buildings/crane.lua
@@ -1,44 +1,77 @@
 RECIPE {
   type = "recipe",
-  name = "crane-1",
+  name = "crane-mk01",
   energy_required = 1,
-  ingredients = {},
+  ingredients = {
+		{type = "item", name = "brain", amount = 1},
+		{type = "item", name = "bio-sample", amount = 15},
+		{type = "item", name = "moss-gen", amount = 20},
+		{type = "item", name = "meat", amount = 15},
+		{type = "item", name = "electronic-circuit", amount = 12},
+		{type = "item", name = "small-parts-01", amount = 10},
+		{type = "item", name = "brake-mk01", amount = 1},
+		{type = "item", name = "gearbox-mk01", amount = 1},
+		{type = "item", name = "shaft-mk01", amount = 2},
+		{type = "item", name = "intermetallics", amount = 5},
+	},
   results = {
-    {
-      type = "item",
-      name = "crane-1",
-      amount = 1
-    }
+    { type = "item", name = "crane-mk01", amount = 1 }
   }
 }
 
-ITEM {
-  type = "item",
-  name = "crane-1",
-  icon = "__pyalienlifegraphics__/graphics/icons/meat.png",
-  icon_size = 32,
-  stack_size = 50,
-  place_result = "crane-1",
+RECIPE {
+  type = "recipe",
+  name = "crane-mk02",
+  energy_required = 1,
+  ingredients = { },
+  results = {
+    { type = "item", name = "crane-mk02", amount = 1 }
+  }
 }
 
-ENTITY {
-  type = "inserter",
-  name = "crane-1",
-  icon = "__pyalienlifegraphics__/graphics/icons/meat.png",
-  icon_size = 32,
-  extension_speed = 0.005,
-  rotation_speed = 0.005,
-  pickup_position = {0, 1.5},
-  insert_position = {0, -1.6},
-  energy_source = {
-    type = "electric",
-    usage_priority = "secondary-input",
-    drain = "500kW",
-  },
-  energy_per_movement = "500kW",
-  energy_per_rotation = "500kW",
-  collision_box = {{-1.4, -0.9}, {1.4, 0.9}},
-  selection_box = {{-1.4, -0.9}, {1.4, 0.9}},
-  uses_inserter_stack_size_bonus = false,
-  stack_size_bonus = 147,
+RECIPE {
+  type = "recipe",
+  name = "crane-mk03",
+  energy_required = 1,
+  ingredients = { },
+  results = {
+    { type = "item", name = "crane-mk03", amount = 1 }
+  }
 }
+
+for i = 1, 3 do
+	local name = "crane-mk0" .. i
+	local icon = "__pyalienlifegraphics__/graphics/icons/meat.png"
+	local icon_size = 32
+
+	ITEM {
+	  type = "item",
+	  name = name,
+	  icon = icon,
+	  icon_size = icon_size,
+	  stack_size = 10,
+	  place_result = name,
+	}
+
+	ENTITY {
+	  type = "inserter",
+	  name = name,
+	  icon = icon,
+	  icon_size = icon_size,
+	  extension_speed = 0.005*i,
+	  rotation_speed = 0.005*i,
+	  pickup_position = {0, 1.5},
+	  insert_position = {0, -1.6},
+	  energy_source = {
+		type = "electric",
+		usage_priority = "secondary-input",
+		drain = 500*i .. "kW",
+	  },
+	  energy_per_movement = 500*i .. "kW",
+	  energy_per_rotation = 500*i .. "kW",
+	  collision_box = {{-1.4, -0.9}, {1.4, 0.9}},
+	  selection_box = {{-1.4, -0.9}, {1.4, 0.9}},
+	  uses_inserter_stack_size_bonus = false,
+	  stack_size_bonus = 147,
+	}
+end
