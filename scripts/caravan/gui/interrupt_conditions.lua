105ac036e59b0936cf14c3817993c6a3dccc7d15
Théo Delrieu <delrieutheo@gmail.com>
2025-07-27 15:28:50 +0200

    caravan/interrupt_conditions: fix at-outpost/not-at-outpost

--- scripts/caravan/gui/interrupt_conditions.lua
+++ scripts/caravan/gui/interrupt_conditions.lua
@@ -8,32 +8,36 @@ function P.build_condition_flow(parent, condition, tags)
     local flow = parent.add {type = "flow", direction = "horizontal"}
     flow.style.vertical_align = "center"
 
-    local label = flow.add {type = "label", style = "squashable_label_with_left_padding", caption = condition.localised_name}
-    if condition.type == "circuit-condition" then
-        comparator.build_circuit_comparator_widgets(flow, condition, tags)
-    elseif condition.type == "circuit-condition-static" then
-        -- whoops, migration fail. https://github.com/pyanodon/pybugreports/issues/880
-        if type(condition.circuit_condition_left) == "number" then
-            condition.circuit_condition_left, condition.circuit_condition_right = condition.circuit_condition_right, condition.circuit_condition_left
-        end
+    if Utils.contains({"at-outpost", "not-at-outpost"}, condition.type) then
+        flow.add {type = "sprite-button", name = "py_edit_interrupt_condition_select_outpost_button", tags = tags, index = 1, style = "train_schedule_action_button", sprite = "utility/rename_icon"}
 
-        comparator.build_circuit_static_comparator_widgets(flow, condition, tags)
-    elseif Utils.contains({"food-count", "caravan-item-count", "target-item-count"}, condition.type) then
-        local filters
-        if condition.type == "food-count" then
-            filters = {{filter = "name", name = Caravan.foods.all}}
+        local locale_key = "caravan-actions." .. condition.type .. "2"
+        local entity = condition.entity
+        local caption
+        if entity and entity.valid then
+            caption = {locale_key, {"caravan-gui.entity-position", entity.localised_name, entity.position.x, entity.position.y}}
+        else
+            caption = {locale_key, {"caravan-gui.not-specified"}}
         end
-        comparator.build_item_static_comparator_widgets(flow, condition, tags, filters)
-    elseif Utils.contains({"at-outpost", "not-at-outpost"}, condition.type) then
-            flow.add {type = "sprite-button", name = "py_add_outpost", tags = tags, index = 1, style = "train_schedule_wait_condition_button", sprite = "utility/rename_icon"}
-            
-            local locale_key = "caravan-actions." .. condition.type .. "2"
-            local entity = condition.entity
-            if entity and entity.valid then
-                label.caption = {locale_key, {"caravan-gui.entity-position", entity.localised_name, entity.position.x, entity.position.y}}
-            else
-                label.caption = {locale_key, {"caravan-gui.not-specified"}}
+        flow.add {type = "label", caption = caption}
+    else
+        flow.add {type = "label", style = "squashable_label_with_left_padding", caption = condition.localised_name}
+        if condition.type == "circuit-condition" then
+            comparator.build_circuit_comparator_widgets(flow, condition, tags)
+        elseif condition.type == "circuit-condition-static" then
+            -- whoops, migration fail. https://github.com/pyanodon/pybugreports/issues/880
+            if type(condition.circuit_condition_left) == "number" then
+                condition.circuit_condition_left, condition.circuit_condition_right = condition.circuit_condition_right, condition.circuit_condition_left
             end
+
+            comparator.build_circuit_static_comparator_widgets(flow, condition, tags)
+        elseif Utils.contains({"food-count", "caravan-item-count", "target-item-count"}, condition.type) then
+            local filters
+            if condition.type == "food-count" then
+                filters = {{filter = "name", name = Caravan.foods.all}}
+            end
+            comparator.build_item_static_comparator_widgets(flow, condition, tags, filters)
+        end
     end
 
     flow.add {type = "empty-widget"}.style.horizontally_stretchable = true
