4423e1a76beaa864232048197899daa0c63d7a16
Szymon Zaborowski <zabor2432@gmail.com>
2025-07-31 20:22:52 +0200

    fixed passing params

--- scripts/caravan/gui/interrupt_conditions.lua
+++ scripts/caravan/gui/interrupt_conditions.lua
@@ -33,7 +33,7 @@ function P.build_condition_flow(parent, condition, tags)
             caption = {locale_key, {"caravan-gui.not-specified"}}
         end
         flow.add {type = "label", caption = caption}
-        comparator.build_item_static_comparator_widgets(flow, condition, tags, filters)
+        comparator.build_item_static_comparator_widgets(flow, condition, tags, nil)
     else
         flow.add {type = "label", style = "squashable_label_with_left_padding", caption = condition.localised_name}
         if condition.type == "circuit-condition" then
