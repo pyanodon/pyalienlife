dc060f49720d15d1c38491aefa41ed54c1b114b4
protocol-1903 <fisher.braydon@yahoo.com>
2025-08-06 22:50:40 -0700

    it is so friendly my dudes

--- migrations/farm-optimizations.lua
+++ migrations/farm-optimizations.lua
@@ -1,91 +1,18 @@
-Farming = {}
+storage.farm_buildings = storage.farm_buildings or {}
 
-local function validate_farm_building_list(farm_buildings_list, throw)
-    local modules = prototypes.get_item_filtered {{filter = "type", type = "module"}}
-    ---@as table<string, table<string, boolean>> two level table containing buildings indexed by their base (mk-less) name
-    local buildings = {}
-    local crafting_machines = prototypes.get_entity_filtered {{filter = "crafting-machine"}}
-    -- This early search and sort lets us avoid o^n searching below
-    for building_name in pairs(crafting_machines) do
-        local basename = building_name:gsub("%-mk..+", "")
-        if farm_buildings_list[basename] then
-            buildings[basename] = buildings[basename] or {}
-            buildings[basename][building_name] = true
-        end
-    end
-
-    -- Assigns nil or errors depending on `throw`
-    local result = throw and error or log
-    for entity_name, farm_prototype in pairs(farm_buildings_list) do
-        -- No buildings with this base name
-        if not buildings[entity_name] then
-            farm_buildings_list[entity_name] = result(("Farm building \"%s\" has no associated crafting machines"):format(entity_name))
-            goto next_farm_prototype
-        end
-        -- No modules with this name
-        if farm_prototype.default_module ~= nil and not modules[farm_prototype.default_module] then
-            farm_buildings_list[entity_name] = result(("Invalid default module \"%s\" for farm building \"%s\""):format(farm_prototype.default_module, entity_name))
-            goto next_farm_prototype
-        end
-        -- Unspecified or invalid domain
-        local domain = farm_prototype.domain
-        if not domain or not (domain == "animal" or domain == "plant" or domain == "fungi") then
-            farm_buildings_list[entity_name] = result(("Invalid domain \"%s\" for farm building \"%s\". Expected 'animal', 'plant', or 'fungi'"):format(domain or "nil", entity_name))
-            goto next_farm_prototype
-        end
-        -- Wow, so valid
-        ::next_farm_prototype::
-    end
+for _, metatable in pairs(storage.enabled_farm_buildings) do
+  
 end
 
----@as table<string, table<string, string>>
----Contains key-value pairs of `{farm_name = {default_module = farm_module, domain = farm_domain, requires = farm_mod}`
--- See `scripts/farming/farm-build-list.lua` for an example
-local farm_buildings_list = require "farm-building-list"
-
----register_type registers a farm for module restrictions
----@param farm_name string name of farm building without -mkxx suffix
----@param domain 'animal' | 'plant' | 'fungi'
----@param default_module string
-function Farming.register_type(farm_name, domain, default_module)
-    log("remote registered farm \'" .. farm_name .. "\' (" .. domain .. ")")
-    storage.farm_prototypes = storage.farm_prototypes or farm_buildings_list
-    storage.farm_prototypes[farm_name] = {default_module = default_module, domain = domain}
-    validate_farm_building_list(storage.farm_prototypes, true)
+for _, metatable in pairs(storage.disabled_farm_buildings) do
+  
 end
 
----unregister_type unregisters a farm for module restrictions
----@param farm_name string name of farm building without -mkxx suffix
-function Farming.unregister_type(farm_name)
-    log("remote unregistered farm \'" .. farm_name .. "\'")
-    storage.farm_prototypes[farm_name] = nil
-end
+storage.enabled_farm_buildings = nil
+storage.disabled_farm_buildings = nil
+storage.next_farm_index = nil
 
-remote.remove_interface("pyfarm")
-remote.add_interface("pyfarm", {
-    register = Farming.register_type,
-    unregister = Farming.unregister_type
-})
 
--- animal, plant, or fungi?
-function Farming.get_kingdom(entity)
-    local name = entity.name:gsub("%-mk..+", "")
-    local farm_data = storage.farm_prototypes[name]
-    if farm_data then return farm_data.domain end
-end
-
-function Farming.get_default_module(entity)
-    local name = entity.name:gsub("%-mk..+", "")
-    local farm_data = storage.farm_prototypes[name]
-    if farm_data then return farm_data.default_module end
-end
-
-py.on_event(py.events.on_init(), function()
-    storage.farm_buildings = storage.farm_buildings or {}
-    storage.farming_deathrattles = storage.farming_deathrattles or {}
-    storage.farm_prototypes = farm_buildings_list
-    validate_farm_building_list(storage.farm_prototypes)
-end)
 
 local function register_sacrifice(manager, farm)
   manager.get_inventory(defines.inventory.crafter_input).insert{
@@ -147,10 +74,11 @@ py.on_event(defines.events.on_object_destroyed, function(event)
 
         -- handle deathrattles
         local metadata = storage.farm_buildings[storage.farming_deathrattles[event.registration_number]]
-        if not metadata then return elseif not (metadata.farm and metadata.farm.valid and metadata.manager and metadata.manager.valid and metadata.monitor and metadata.monitor.valid) then
+        if not metadata then return elseif not (metadata.farm and metadata.farm.valid and metadata.manager and metadata.manager.valid and metadata.monitor and metadata.monitor.valid) or (metadata.warning and not metadata.monitor.valid) then
             if metadata.farm and metadata.farm.valid then metadata.farm.destroy() end
             if metadata.manager and metadata.manager.valid then metadata.manager.destroy() end
             if metadata.monitor and metadata.monitor.valid then metadata.monitor.destroy() end
+            if metadata.warning and metadata.warning.valid then metadata.farm.destroy() end
             storage.farm_buildings[storage.farming_deathrattles[event.registration_number]] = nil
             storage.farming_deathrattles[event.registration_number] = nil
             return
@@ -194,6 +122,7 @@ py.on_event(defines.events.on_object_destroyed, function(event)
         -- destroy support entities and remove storage reference
         metadata.manager.destroy()
         metadata.monitor.destroy()
+        if metadata.warning then metadata.warning.destroy() end
         storage.farm_buildings[event.useful_id] = nil
     end
 end)
---
+++ prototypes/farming-utils.lua
@@ -0,0 +1,74 @@
+data:extend{ -- entities and things to manage farming buildings
+  {
+    type = "recipe-category",
+    name = "pyfarm-filler-category",
+    hidden = true,
+    hidden_in_factoriopedia = true
+  },
+  { -- hidden recipe used to check if machine is working
+    type = "recipe",
+    name = "pyfarm-internal-recipe",
+    icon = util.empty_icon().icon,
+    category = "pyfarm-filler-category",
+    ingredients = {{ type = "item", name = "pyfarm-internal-item", amount = 1, ignored_by_stats = 1}},
+    hidden = true,
+    hidden_in_factoriopedia = true
+  },
+  { -- hidden item for recipe and signals, can use existing item but this one is garunteed to work
+    type = "item",
+    name = "pyfarm-internal-item",
+    icon = util.empty_icon().icon,
+    stack_size = 1,
+    hidden = true,
+    hidden_in_factoriopedia = true
+  },
+  { -- hidden assembling machine to craft the aforementioned recipe
+    type = "assembling-machine",
+    name = "pyfarm-internal-manager",
+    icon = util.empty_icon().icon,
+    collision_mask = {layers = {}},
+    flags = {
+      "placeable-off-grid",
+      "not-repairable",
+      "not-on-map",
+      "not-blueprintable",
+      "not-deconstructable",
+      "no-copy-paste",
+      "not-upgradable",
+      "placeable-neutral",
+      "no-automated-item-removal",
+      "no-automated-item-insertion"
+    },
+    allow_copy_paste = false,
+    selectable_in_game = false,
+    energy_usage = "1W",
+    energy_source = {type = "void"},
+    crafting_categories = {"pyfarm-filler-category"},
+    fixed_recipe = "pyfarm-internal-recipe",
+    crafting_speed = 60,
+    hidden = true,
+    hidden_in_factoriopedia = true
+  },
+  { -- hidden proxy container to monitor module inventory
+    type = "proxy-container",
+    name = "pyfarm-internal-monitor",
+    icon = util.empty_icon().icon,
+    draw_inventory_content = false,
+    collision_mask = {layers = {}},
+    flags = {
+      "not-rotatable",
+      "placeable-neutral",
+      "placeable-off-grid",
+      "not-repairable",
+      "not-on-map",
+      "not-deconstructable",
+      "not-blueprintable",
+      "hide-alt-info",
+      "not-upgradable"
+    },
+    allow_copy_paste = false,
+    selectable_in_game = false,
+    hidden = true,
+    hidden_in_factoriopedia = true
+  }
+}
