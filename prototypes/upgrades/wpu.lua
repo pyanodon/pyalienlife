pyanodon/pyalienlife

commit 9d5674501ea175aa17927ad318921fdb59bd62e2

WPU and Fish farm normalization (#364)

prototypes/upgrades/wpu.lua
@@ -87,21 +87,21 @@ if data and not yafc_turd_integration then
             recipe:add_ingredient(ingredient)
         end
 
-        local name = "wpu" .. ((i == 1) and "" or ("-mk0" .. i))
+        local name = "wpu-mk0" .. i
         data.raw["assembling-machine"][name].allowed_effects = {"consumption", "speed", "productivity"}
         local entity = table.deepcopy(data.raw["assembling-machine"][name])
-        entity.name = "turd-" .. name
+        entity.name = name .. "-turd"
         entity.localised_name = {"entity-name." .. name}
         entity.placeable_by = {item = name, count = 1}
         entity.localised_description = entity.localised_description or {"entity-description." .. name}
         entity.subgroup = data.raw.item[name].subgroup
         entity.order = data.raw.item[name].order
         entity.allowed_module_categories = {"sawblade"}
-        if i ~= 4 then entity.next_upgrade = "turd-wpu-mk0" .. (i + 1) end
+        if i ~= 4 then entity.next_upgrade = "wpu-mk0" .. (i + 1) .. "-turd" end
         table.insert(entity.flags, "not-in-made-in")
         entity.module_slots = MODULE_SLOTS
         entity.allowed_effects = {"speed", "productivity", "pollution"}
-        entity.crafting_speed = i == 1 and py.farm_speed(MODULE_SLOTS, FULL_CRAFTING_SPEED) or py.farm_speed_derived(MODULE_SLOTS, "turd-wpu")
+        entity.crafting_speed = py.farm_speed(MODULE_SLOTS, FULL_CRAFTING_SPEED) or py.farm_speed_derived(MODULE_SLOTS, "wpu-turd")
         data:extend {entity}
     end
 
@@ -226,7 +226,7 @@ end
 
 return {
     affected_entities = { -- the entities that should be effected by this tech upgrade
-        "wpu",
+        "wpu-mk01",
         "wpu-mk02",
         "wpu-mk03",
         "wpu-mk04",
@@ -268,10 +268,10 @@ return {
                 {type = "unlock-recipe",       recipe = "py-sawblade-module-mk02"},
                 {type = "unlock-recipe",       recipe = "py-sawblade-module-mk03"},
                 {type = "unlock-recipe",       recipe = "py-sawblade-module-mk04"},
-                {type = "machine-replacement", old = "wpu",                       new = "turd-wpu"},
-                {type = "machine-replacement", old = "wpu-mk02",                  new = "turd-wpu-mk02"},
-                {type = "machine-replacement", old = "wpu-mk03",                  new = "turd-wpu-mk03"},
-                {type = "machine-replacement", old = "wpu-mk04",                  new = "turd-wpu-mk04"},
+                {type = "machine-replacement", old = "wpu-mk01",                       new = "wpu-mk01-turd"},
+                {type = "machine-replacement", old = "wpu-mk02",                  new = "wpu-mk02-turd"},
+                {type = "machine-replacement", old = "wpu-mk03",                  new = "wpu-mk03-turd"},
+                {type = "machine-replacement", old = "wpu-mk04",                  new = "wpu-mk04-turd"},
             },
         },
         {

prototypes/updates/pycoalprocessing-updates.lua
@@ -44,13 +44,13 @@ RECIPE("washer"):remove_unlock("energy-1").enabled = true
 RECIPE("py-gas-vent"):add_unlock("coal-processing-1").enabled = false
 RECIPE("tailings-pond"):add_unlock("coal-processing-1").enabled = false
 
-table.insert(data.raw["assembling-machine"]["wpu"].crafting_categories, "wpu-handcrafting")
+table.insert(data.raw["assembling-machine"]["wpu-mk01"].crafting_categories, "wpu-handcrafting")
 table.insert(data.raw["assembling-machine"]["wpu-mk02"].crafting_categories, "wpu-handcrafting")
 table.insert(data.raw["assembling-machine"]["wpu-mk03"].crafting_categories, "wpu-handcrafting")
 table.insert(data.raw["assembling-machine"]["wpu-mk04"].crafting_categories, "wpu-handcrafting")
 
 if not mods.pystellarexpedition then
-    table.insert(data.raw["assembling-machine"]["wpu-legacy"].crafting_categories, "wpu-handcrafting")
+    table.insert(data.raw["assembling-machine"]["wpu-mk01-legacy"].crafting_categories, "wpu-handcrafting")
     table.insert(data.raw["assembling-machine"]["wpu-mk02-legacy"].crafting_categories, "wpu-handcrafting")
     table.insert(data.raw["assembling-machine"]["wpu-mk03-legacy"].crafting_categories, "wpu-handcrafting")
     table.insert(data.raw["assembling-machine"]["wpu-mk04-legacy"].crafting_categories, "wpu-handcrafting")
@@ -98,7 +98,7 @@ RECIPE("fawogae-substrate"):remove_ingredient("fawogae"):add_ingredient {type = 
 RECIPE("bio-sample01"):add_ingredient {type = "item", name = "petri-dish-bacteria", amount = 2}:add_ingredient {type = "item", name = "native-flora", amount = 12}
 RECIPE("fawogae-substrate"):add_unlock("basic-substrate").enabled = false
 
-RECIPE("wpu"):replace_ingredient("wood", {type = "item", name = "log", amount = 5}):remove_unlock("wood-processing")
+RECIPE("wpu-mk01"):replace_ingredient("wood", {type = "item", name = "log", amount = 5}):remove_unlock("wood-processing")
 RECIPE("log-wood"):remove_unlock("wood-processing"):set_fields {always_show_made_in = true}.category = "wpu-handcrafting"
 RECIPE("botanical-nursery"):replace_ingredient("wood", {type = "item", name = "planter-box", amount = 10})
 

prototypes/upgrades/fish.lua
@@ -55,13 +55,13 @@ end
 local function add_new_fish_farm(i)
     local name = "fish-farm-mk0" .. i
     local entity = table.deepcopy(data.raw["assembling-machine"][name])
-    entity.name = "turd-" .. name
+    entity.name = name .. "-turd"
     entity.localised_name = {"entity-name." .. name}
     entity.placeable_by = {item = name, count = 1}
     entity.localised_description = entity.localised_description or {"entity-description." .. name}
     entity.subgroup = data.raw.item[name].subgroup
     entity.order = data.raw.item[name].order
-    if i ~= 4 then entity.next_upgrade = "turd-fish-farm-mk0" .. (i + 1) end
+    if i ~= 4 then entity.next_upgrade = "fish-farm-mk0" .. (i + 1) .. "-turd" end
     entity.allowed_module_categories = {"fish"}
     entity.energy_usage = (i * 5) .. "MW"
     table.insert(entity.flags, "not-in-made-in")
@@ -211,10 +211,10 @@ return {
             icon_size = 128,
             order = "c-a",
             effects = { -- the effects the tech will have on the building. valid types: 'module-effects', 'unlock-recipe', 'recipe-replacement', 'machine-replacement'
-                {type = "machine-replacement", old = "fish-farm-mk01",          new = "turd-fish-farm-mk01"},
-                {type = "machine-replacement", old = "fish-farm-mk02",          new = "turd-fish-farm-mk02"},
-                {type = "machine-replacement", old = "fish-farm-mk03",          new = "turd-fish-farm-mk03"},
-                {type = "machine-replacement", old = "fish-farm-mk04",          new = "turd-fish-farm-mk04"},
+                {type = "machine-replacement", old = "fish-farm-mk01",          new = "fish-farm-mk01-turd"},
+                {type = "machine-replacement", old = "fish-farm-mk02",          new = "fish-farm-mk02-turd"},
+                {type = "machine-replacement", old = "fish-farm-mk03",          new = "fish-farm-mk03-turd"},
+                {type = "machine-replacement", old = "fish-farm-mk04",          new = "fish-farm-mk04-turd"},
                 {old = "breed-fish-egg-1",     new = "breed-fish-egg-1-doused", type = "recipe-replacement"},
                 {old = "breed-fish-egg-2",     new = "breed-fish-egg-2-doused", type = "recipe-replacement"},
                 {old = "breed-fish-egg-3",     new = "breed-fish-egg-3-doused", type = "recipe-replacement"},

scripts/farming/farm-building-list.lua
@@ -7,7 +7,7 @@ local farm_buildings = {
     ["dhilmos-pool"] = {default_module = "dhilmos", domain = "animal"},
     ["dingrits-pack"] = {default_module = "dingrits", domain = "animal"},
     ["fish-farm"] = {default_module = "fish", domain = "animal"},
-    ["turd-fish-farm"] = {default_module = "fish", domain = "animal"},
+    ["fish-farm-turd"] = {default_module = "fish", domain = "animal"},
     ["kmauts-enclosure"] = {default_module = "kmauts", domain = "animal"},
     ["mukmoux-pasture"] = {default_module = "mukmoux", domain = "animal"},
     ["phadai-enclosure"] = {default_module = "phadai", domain = "animal"},

commit f7f2e11795ffe150e1d1a8474907d8279abdd4f6

Wpu fix (#404)
* Use derived speed for mk2+
fixes regression introduced in #364

prototypes/upgrades/wpu.lua
@@ -101,7 +101,7 @@ if data and not yafc_turd_integration then
         table.insert(entity.flags, "not-in-made-in")
         entity.module_slots = MODULE_SLOTS
         entity.allowed_effects = {"speed", "productivity", "pollution"}
-        entity.crafting_speed = py.farm_speed(MODULE_SLOTS, FULL_CRAFTING_SPEED) or py.farm_speed_derived(MODULE_SLOTS, "wpu-turd")
+        entity.crafting_speed = i == 1 and py.farm_speed(MODULE_SLOTS, FULL_CRAFTING_SPEED) or py.farm_speed_derived(MODULE_SLOTS, "wpu-mk01-turd")
         data:extend {entity}
     end
 