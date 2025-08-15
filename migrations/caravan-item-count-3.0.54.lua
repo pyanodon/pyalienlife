19e35020486435abade033d951c289bfe2b6e29f
oorzkws <65210810+oorzkws@users.noreply.github.com>
2025-08-15 16:43:49 -0600

    caravans: fix circuit-condition-static comparator

--- migrations/caravan-item-count-3.0.54.lua
+++ migrations/caravan-item-count-3.0.54.lua
@@ -1,9 +1,25 @@
-    for _, interrupt_data in pairs(storage.interrupts or {}) do
-        for _, condition in pairs(interrupt_data.conditions or {}) do
-            -- migrate broken conditions
-            if condition.type:find("%-count$") and type(condition.circuit_condition_right) == "number" then
-                condition.item_count = condition.circuit_condition_right
-                condition.circuit_condition_right = nil
+for _, interrupt_data in pairs(storage.interrupts or {}) do
+    for _, condition in pairs(interrupt_data.conditions or {}) do
+        -- migrate broken conditions
+        if (condition.type == "circuit-condition-static" or condition.type:find("%-count$")) and type(condition.circuit_condition_right) == "number" then
+            condition.item_count = condition.circuit_condition_right
+            condition.circuit_condition_right = nil
+        end
+    end
+end
+for _, caravan_data in pairs(storage.caravans or {}) do
+    for _, schedule in pairs(caravan_data.schedule or {}) do
+        for _, action in pairs(schedule.actions or {}) do
+            if action.type == "circuit-condition-static" then
+                -- previous migration fail. https://github.com/pyanodon/pybugreports/issues/880
+                if type(action.circuit_condition_left) == "number" then
+                    action.circuit_condition_left, action.circuit_condition_right = action.circuit_condition_right, action.circuit_condition_left
+                end
+                if type(action.circuit_condition_right) == "number" then
+                    action.item_count = action.circuit_condition_right
+                    action.circuit_condition_right = nil
+                end
             end
         end
     end
+end
