253d2bcacf7376247ac7eb5c8e40d33187eeee3c
Alyssa Doubrovsky <adoubrovsky@gmail.com>
2025-08-15 20:12:31 -0700

    Outposts now show remote view of all connected caravans

--- scripts/caravan/manager.lua
+++ scripts/caravan/manager.lua
@@ -258,3 +258,96 @@ remote.add_interface("pywiki_caravan_manager", {
     create_gui = create_gui,
     on_search = on_search,
 })
+
+local relative_gui_types = {
+    ["electric-pole"] = "electric_network_gui",
+    ["character"] = "other_player_gui",
+    ["unit"] = "script_inventory_gui"
+}
+
+
+local function guess(entity)
+    local entity_type = entity.type
+    local relative_gui_type = relative_gui_types[entity_type] or entity_type:gsub("%-", "_") .. "_gui"
+    return defines.relative_gui_type[relative_gui_type] or defines.relative_gui_type.generic_on_off_entity_gui
+end
+
+--checks to see if any caravan has this entity in it's schedule
+function has_any_caravan(entity)
+    for _, caravan_data in pairs(storage.caravans) do
+        if Caravan.has_entity_in_schedule(caravan_data, entity) then return true end
+    end
+    return false
+end
+
+--checks to see if the given caravan has the given entity in it's schedule
+function has_entity_in_schedule(caravan_data, entity)
+    if not Impl.validity_check(caravan_data) then return end
+    if not caravan_data.schedule then return end
+    for _, schedule in pairs(caravan_data.schedule) do
+        if schedule.entity == entity then return true end
+    end
+    return false
+end
+
+--creates the main from of the remote view gui
+local function instantiate_main_frame(gui, anchor)
+    if anchor then
+        return gui.relative.add {
+            type = "frame",
+            name = "py_global_caravan_gui",
+            caption = {"caravan-gui.caption"},
+            direction = "vertical",
+            anchor = anchor
+        }
+    end
+    if not gui.relative.caravan_flow then return end
+        gui.relative.caravan_flow.clear()
+    return gui.relative.caravan_flow.add {
+        type = "frame",
+        name = "py_global_caravan_gui",
+        caption = {"caravan-gui.caption"},
+        direction = "vertical",
+    }
+end
+
+--attempts to add a remote view for caravans connected to the given outpost
+local function build_gui_connected(player, entity, anchor)
+    if not entity then return end
+    if not has_any_caravan(entity) then return end
+    local main_frame = instantiate_main_frame(player.gui, anchor)
+    if not main_frame then return end
+    main_frame.style.minimal_width = 300
+    main_frame.tags = {unit_number = entity.unit_number}
+
+    local scroll_pane = main_frame.add {type = "scroll-pane"}
+    scroll_pane.style.top_margin = -6
+
+    for key, caravan_data in pairs(storage.caravans) do
+        if has_entity_in_schedule(caravan_data, entity) then
+            scroll_pane.add {type = "empty-widget"}.style.height = 3
+            add_gui_row(caravan_data, key, scroll_pane)
+        end
+    end
+end
+
+py.on_event(defines.events.on_gui_opened, function(event)
+    local player = game.get_player(event.player_index)
+    local entity = event.entity
+    if not entity then return end
+    if player.gui.relative.connected_caravan_gui then return end
+    local anchor = {
+        gui = guess(entity),
+        position = defines.relative_gui_position.right
+    }
+    build_gui_connected(player, entity, anchor)
+end)
+
+--removes the remote caravan view when closing an outpost
+py.on_event({defines.events.on_gui_closed, defines.events.on_player_changed_surface}, function(event)
+    local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+
+    if player.gui.relative.py_global_caravan_gui then
+        player.gui.relative.py_global_caravan_gui.destroy()
+    end
+end)
