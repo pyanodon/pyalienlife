d203401d709a8145e902fa10f1876097521fd367
oorzkws <65210810+oorzkws@users.noreply.github.com>
2025-08-17 15:35:29 -0600

    Switch update warning to use locale string, include mod name

--- migrations/3.0.54.lua
+++ migrations/3.0.54.lua
@@ -1,6 +1,6 @@
 for _, surface in pairs(game.surfaces) do
     if #surface.find_entities_filtered{name = {"auog-paddock-mk02", "auog-paddock-mk03", "auog-paddock-mk04"}} > 0 then
-        game.print("[color=yellow]Note:[/color] [entity=auog-paddock-mk02][entity=auog-paddock-mk03][entity=auog-paddock-mk04] had a small speed increase (~2%) this update, you may wish to adjust your building ratios.")
+        game.print({"command-output.update-note", {"mod-name.pyalienlife"}, "[entity=auog-paddock-mk02][entity=auog-paddock-mk03][entity=auog-paddock-mk04] had a small speed increase (~2%) this update, you may wish to adjust your building ratios."})
         break
     end
 end
