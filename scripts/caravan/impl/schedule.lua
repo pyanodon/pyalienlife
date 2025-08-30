9ac25af091bc4bf2f5416a8b908a12c66e04f3e4
oorzkws <65210810+oorzkws@users.noreply.github.com>
2025-08-30 15:10:21 -0600

    caravans: fix empty interrupts cancelling other interrupts below it

--- scripts/caravan/impl/schedule.lua
+++ scripts/caravan/impl/schedule.lua
@@ -56,9 +56,9 @@ function P.find_interrupt_to_trigger(caravan_data)
 
     for _, interrupt_name in pairs(caravan_data.interrupts) do
         local interrupt = storage.interrupts[interrupt_name]
-        -- TODO shouldn't we assert? Is it a check for multiplayer shenanigans?
-        if interrupt then
-
+        -- TODO: shouldn't we assert `interrupt`? Is it a check for multiplayer shenanigans?
+        -- if there's no *actions* (schedule), we don't care about trying the conditions.
+        if interrupt and interrupt.schedule and #interrupt.schedule > 0 then
             if not current_schedule or temporary_schedule == nil or interrupt.inside_interrupt then
                 if P.evaluate_conditions(caravan_data, interrupt) then
                     return interrupt
@@ -133,9 +133,12 @@ function P.advance_caravan_schedule_by_1(caravan_data)
 
     local interrupt = P.find_interrupt_to_trigger(caravan_data)
 
-    if interrupt and #interrupt.schedule > 0 then
+    if interrupt then
         P.trigger_interrupt_in_regular_schedule(caravan_data, interrupt)
     else
+        if interrupt and #interrupt.schedule == 0 then
+            log("skipping because empty schedule")
+        end
         if caravan_data.schedule[caravan_data.schedule_id].temporary then
             table.remove(caravan_data.schedule, caravan_data.schedule_id)
         else
