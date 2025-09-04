313aece67d3cfc63858fa5e2c641e77409c66dc0
Wiwiweb <wiwiweb2@gmail.com>
2025-09-03 23:30:11 -0700

    Add keyboard shortcuts for quick codex opening

---
+++ prototypes/keyboard-shortcuts.lua
@@ -0,0 +1,14 @@
+data:extend{
+    {
+        type = "custom-input",
+        name = "py_toggle_caravan_manager_gui",
+        key_sequence = "CONTROL + ALT + C",
+        order = "a"
+    },
+    {
+        type = "custom-input",
+        name = "py_toggle_turd_gui",
+        key_sequence = "CONTROL + ALT + T",
+        order = "b"
+    },
+}
