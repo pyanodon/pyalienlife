f49f54d78c62f5adb5c3d552c5b19c7938da1a79
Zachary Picco <zacharypicco@gmail.com>
2025-09-08 12:27:02 -0500

    Fixed tech loop caused by xenobiology.

--- prototypes/technologies/biotech.lua
+++ prototypes/technologies/biotech.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/biotech-mk01.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"xenobiology"},
+    prerequisites = {},
     effects = {},
     unit = {
         count = 150,
