543469e333ae5c4a93ef757d0b5203c3b2e71530
Zachary Picco <zacharypicco@gmail.com>
2025-09-08 12:02:47 -0500

    Removed technology loop caused by botany mk2, mukmoux, and tuuphras.

--- prototypes/technologies/korlex.lua
+++ prototypes/technologies/korlex.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/korlex.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"tuuphra", "biotech-mk02", "mukmoux"},
+    prerequisites = {"tuuphra"},
     effects = {},
     unit = {
         count = 230,
