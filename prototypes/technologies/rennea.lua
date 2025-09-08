7815d0cd92f2658a914295e41f86f58a83e9c3e4
Zachary Picco <zacharypicco@gmail.com>
2025-09-08 12:39:20 -0500

    Fixed tech loop caused by rennea.

--- prototypes/technologies/rennea.lua
+++ prototypes/technologies/rennea.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/rennea.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"botany-mk02"},
+    prerequisites = {},
     effects = {},
     unit = {
         count = 150,
