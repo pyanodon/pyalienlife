dc653c32d40a345486614b64369259d54c2ac618
charleycrissman <charleyc@gmail.com>
2026-02-22 11:34:39 -0800

    Add "fill/empty until" actions for fluid caravans (#548)
    Adds new actions to fluid caravans similar to the "load/empty until"
    actions for fluid caravans:
    - Fill fluid until (caravan/target) has specific fluid level
    - Empty fluid until (caravan/target) has specific fluid level
    
    Also resolves https://github.com/pyanodon/pybugreports/issues/1375 .

--- scripts/caravan/caravan-prototypes.lua
+++ scripts/caravan/caravan-prototypes.lua
@@ -45,7 +45,11 @@ Caravan.all_actions = {
             "fill-tank",
             "empty-tank",
             "circuit-condition",
-            "circuit-condition-static"
+            "circuit-condition-static",
+            "fill-tank-until-caravan-has",
+            "fill-tank-until-target-has",
+            "empty-tank-until-caravan-has",
+            "empty-tank-until-target-has",
         },
         ["character"] = table.invert {
             "time-passed",
@@ -194,7 +198,11 @@ Caravan.valid_actions = {
             "fill-tank",
             "empty-tank",
             "circuit-condition",
-            "circuit-condition-static"
+            "circuit-condition-static",
+            "fill-tank-until-caravan-has",
+            "fill-tank-until-target-has",
+            "empty-tank-until-caravan-has",
+            "empty-tank-until-target-has",
         },
         ["electric-pole"] = table.invert{
             "time-passed",
@@ -241,6 +249,10 @@ Caravan.actions_with_item_count = table.invert{
     "outpost-fluid-count",
     "caravan-fluid-count",
     "target-fluid-count",
+    "fill-tank-until-caravan-has",
+    "fill-tank-until-target-has",
+    "empty-tank-until-caravan-has",
+    "empty-tank-until-target-has",
 }
 
 Caravan.foods = {
