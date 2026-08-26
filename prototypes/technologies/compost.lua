cba57957c074a982853d73b721d5fd52214df18e
protocol-1903 <67478786+protocol-1903@users.noreply.github.com>
2026-08-25 18:48:59 -0700

    Fixed that Composting did not require Py Science Pack 1 to be researched. Resolves https://github.com/pyanodon/pybugreports/issues/1402 (#616)

--- prototypes/technologies/compost.lua
+++ prototypes/technologies/compost.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pyalienlifegraphics__/graphics/technology/compost.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"biotech-mk01"},
+    prerequisites = {"biotech-mk01", "py-science-pack-1"},
     effects = {},
     unit = {
         count = 200,
