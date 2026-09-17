8b9b1e2498ab436bc746078a2540b9b9fea13376
JeromeJ <e-warning@hotmail.com>
2026-09-17 15:45:34 +0200

    turd.ajusted-speed appears unlocalized in the game

--- prototypes/turd.lua
+++ prototypes/turd.lua
@@ -135,7 +135,7 @@ local function build_module_effects_turd(tech_upgrade, sub_tech, effect)
     if effective_speed then
         local adjusted_speed = effect.speed * 100
         if adjusted_speed >= 0 then adjusted_speed = "+" .. adjusted_speed end
-        py.add_to_description(module, "turd.adjusted-speed", tostring(adjusted_speed))
+        py.add_to_description(module, {"turd.adjusted-speed"}, tostring(adjusted_speed))
     end
 
     if is_this_a_speed_module_that_effects_farm_buildings then
